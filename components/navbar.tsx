d9faa4f5a377ccf3786509d613d63f79f9643c65
jlachor <jlachor@exbet.io>
2023-07-19 11:19:41 +0200

    Upgrades in Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,15 @@
-
+import Link from "next/link"
+import { Input } from "./ui/input"
 
 export default function Navbar () {
   return (
-    <div className=" w-full">Navbar</div>
+    <div className="flex">
+
+      <Link href={"/"}>
+        Witnesses
+      </Link>
+
+      <Input type="text" placeholder="Search user, block, transaction" />
+    </div>
   )
 }
