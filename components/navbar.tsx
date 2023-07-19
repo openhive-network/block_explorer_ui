3b72d86a43d25a20a0f0030950f22c3529e9bff8
jlachor <jlachor@exbet.io>
2023-07-19 11:36:00 +0200

    Add proto navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link"
 import { Input } from "./ui/input"
+import Image from 'next/image'
 
 export default function Navbar () {
   return (
-    <div className="flex">
+    <div className="flex p-2 justify-between bg-explorer-dark-gray text-white	items-center">
 
+    <div className="flex items-center	pl-12">
+      <Link href={"/"} className="pr-12">
+        <Image src="/hive-logo.png" alt="Hive logo" width={50} height={50} />
+      </Link>
       <Link href={"/"}>
         Witnesses
       </Link>
+    </div>
 
-      <Input type="text" placeholder="Search user, block, transaction" />
+
+      <Input className="w-1/3" type="text" placeholder="Search user, block, transaction" />
     </div>
   )
 }
