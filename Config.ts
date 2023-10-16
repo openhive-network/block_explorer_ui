9e3f57f4b39ea6a12429cc12e52e87d7390d791b
jlachor <jlachor@syncad.com>
2023-10-16 11:39:17 +0200

    Switch to steem10

--- Config.ts
+++ Config.ts
@@ -2,7 +2,7 @@ import env from "@beam-australia/react-env"
 
 export const config = {
   hiveBlogAdress: "https://api.hive.blog",
-  // apiAdress: process.env.NEXT_PUBLIC_API_ADDRESS ? process.env.NEXT_PUBLIC_API_ADDRESS : "http://192.168.3.107:3000",
-  apiAdress: `${env('API_ADDRESS') ? env('API_ADDRESS') : 'http://192.168.3.107:3000'}`,
+  // apiAdress: process.env.NEXT_PUBLIC_API_ADDRESS ? process.env.NEXT_PUBLIC_API_ADDRESS : "http://192.168.4.250:3000",
+  apiAdress: `${env('API_ADDRESS') ? env('API_ADDRESS') : 'http://192.168.4.250:3000'}`,
   baseMomentTimeFormat: "DD/MM/YYYY hh:mm:ss"
 }
