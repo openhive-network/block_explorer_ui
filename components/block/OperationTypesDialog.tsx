d5c03970755a861f383014dc2ef80766ed8c7d93
jlachor <jlachor@syncad.com>
2023-10-23 11:32:49 +0200

    Separate virtual operations in dialog

--- components/block/OperationTypesDialog.tsx
+++ components/block/OperationTypesDialog.tsx
@@ -57,6 +57,33 @@ const OperationTypesDialog: React.FC<OperationTypesDialogProps> = ({
     setIsOpen(open);
   }
 
+  const renderOperation = (operation: Hive.OperationPattern) => {
+    return (
+      <li
+        onClick={() => onFiltersSelect(operation.op_type_id)}
+        key={operation.op_type_id}
+        className="col-span-3 pl-2 md:col-span-1 flex items-center font-bold text-base rounded-lg bg-inherit hover:border-2-gray group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white "
+      >
+        <Input
+          type="checkbox"
+          checked={selectedOperationsIds.includes(operation.op_type_id)}
+          name="bordered-checkbox"
+          className=" w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 "
+          onChange={() => onFiltersSelect(operation.op_type_id)}
+        />
+        <Label
+          htmlFor="bordered-checkbox-1"
+          className="p-2 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap overflow-hidden text-ellipsis"
+        >
+          {operation.operation_name}
+        </Label>
+      </li>
+    )
+  }
+
+  const virtualOperations = operationTypes.filter((operationType) => operationType.is_virtual);
+  const nonVirtualOperations = operationTypes.filter((operationType) => !operationType.is_virtual);
+
   return (
     <Dialog open={isOpen} onOpenChange={(open) => {onOpenChange(open)}}>
       <DialogTrigger asChild>
@@ -64,36 +91,20 @@ const OperationTypesDialog: React.FC<OperationTypesDialogProps> = ({
           {triggerTitle}
         </Button>
       </DialogTrigger>
-      <DialogContent className="max-w-[80%] h-[80%] flex-column justify-center align-center  bg-white text-black">
+      <DialogContent className="max-w-[80%] h-[80%] flex-column justify-center align-center  bg-white text-black ">
         <DialogHeader>
           <DialogTitle className="flex justify-center pt-2">Operation Types</DialogTitle>
         </DialogHeader>
+      <div className="overflow-auto">
 
-        <ul className="my-4 grid grid-cols-3 gap-4 place-items-stretch text-white overflow-auto ">
-          {operationTypes.map((operation) => {
-            return (
-              <li
-                onClick={() => onFiltersSelect(operation.op_type_id)}
-                key={operation.op_type_id}
-                className="col-span-3 pl-2 md:col-span-1 flex items-center font-bold text-base rounded-lg bg-inherit hover:border-2-gray group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white "
-              >
-                <Input
-                  type="checkbox"
-                  checked={selectedOperationsIds.includes(operation.op_type_id)}
-                  name="bordered-checkbox"
-                  className=" w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 "
-                  onChange={() => onFiltersSelect(operation.op_type_id)}
-                />
-                <Label
-                  htmlFor="bordered-checkbox-1"
-                  className="p-2 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap overflow-hidden text-ellipsis"
-                >
-                  {operation.operation_name}
-                </Label>
-              </li>
-            );
-          })}
+        <ul className="my-4 grid grid-cols-3 gap-4 place-items-stretch text-white ">
+          {nonVirtualOperations.map((operation) => renderOperation(operation))}
         </ul>
+        <div className="text-center mt-8">Virtual operations</div>
+        <ul className="my-4 grid grid-cols-3 gap-4 place-items-stretch text-white ">
+          {virtualOperations.map((operation) => renderOperation(operation))}
+        </ul>
+      </div>
         <DialogFooter>
           <div className="flex">
           <Button type="button" variant='secondary' onClick={() => {onOpenChange(false)}}>
