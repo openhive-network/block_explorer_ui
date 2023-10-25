4e8d2b4914c6986b7fba58a24dc5597f88afaccd
jlachor <jlachor@syncad.com>
2023-10-25 09:10:22 +0200

    Repair hover table color #86

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -55,7 +55,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      " transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      " transition-colors hover:bg-white data-[state=selected]:bg-muted",
       className
     )}
     {...props}
