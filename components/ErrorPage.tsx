4009c74318d52f2931f9fcf313a111b787934bcb
Piotr Berezka <berezkapiotr@gmail.com>
2023-10-31 10:35:40 +0100

    add error boundaries and error page

---
+++ components/ErrorPage.tsx
@@ -0,0 +1,32 @@
+import { useRouter } from "next/router";
+import React from "react";
+import { Button } from "./ui/button";
+
+const ErrorPage: React.FC = () => {
+  const router = useRouter();
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="">
+        An error occured
+      </div>
+      <div className="flex mt-10 gap-x-8">
+        <Button
+          variant={"outline"}
+          className="hover:bg-explorer-bg-start"
+          onClick={() => location.reload()}
+        >
+          Reload Page
+        </Button>
+        <Button
+          variant={"outline"}
+          className="bg-explorer-yellow hover:bg-explorer-bg-start"
+          onClick={() => {router.push("/").then(() => location.reload())}}
+        >
+          Go To Home Page
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import type { AppProps } from "next/app";
 import Context from "@/components/context";
 import Providers from "@/components/providers";
 import "@/styles/globals.css";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorPage from "@/components/ErrorPage";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Context>
-      <Providers>
-        <Component {...pageProps} />
-      </Providers>
-    </Context>
+    <ErrorBoundary fallback={<ErrorPage />}>
+      <Context>
+        <Providers>
+          <Component {...pageProps} />
+        </Providers>
+      </Context>
+    </ErrorBoundary>
   );
 }
