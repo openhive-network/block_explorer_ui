5fafac7e51d03cb02cdff3328e2c64f9cba229bd
Piotr Berezka <berezkapiotr@gmail.com>
2023-12-01 13:16:10 +0100

    use comments hook in comment page

--- pages/comments.tsx
+++ pages/comments.tsx
@@ -1,30 +1,38 @@
+import useCommentSearch from "@/api/common/useCommentSearch";
+import DetailedOperationCard from "@/components/DetailedOperationCard";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Explorer from "@/types/Explorer";
+import { Loader2 } from "lucide-react";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 
 const Comments: React.FC = () => {
   const [accountName, setAccountName] = useState<string>();
   const [permlink, setPermlink] = useState<string>();
-  const [fromBlock, setFromBlock] = useState<string>();
-  const [toBlock, setToBlock] = useState<string>();
-  const router = useRouter();
+  const [fromBlock, setFromBlock] = useState<number>();
+  const [toBlock, setToBlock] = useState<number>();
+  const commentSearch = useCommentSearch();
+  const [previousCommentSearchProps, setPreviousCommentSearchProps] = useState<Explorer.CommentSearchProps | undefined>(undefined);
+
+  const startCommentSearch = () => {
+    if (accountName) {
+      const commentSearchProps: Explorer.CommentSearchProps = {
+        accountName,
+        permlink,
+        fromBlock,
+        toBlock
+      };
+      commentSearch.searchCommentOperations(commentSearchProps);
+      setPreviousCommentSearchProps(commentSearchProps);
+    }
+  }
+
+  console.log(commentSearch)
 
   return (
-    <div>
-      <div className="bg-explorer-dark-gray text-white p-4 rounded-[6px]">
-        <div className="flex items-center m-2 ">
-          {/* <OperationTypesDialog
-            operationTypes={operationsTypes.filter((opType) =>
-              config.commentOperationsTypeIds.includes(opType.op_type_id)
-            )}
-            selectedOperations={selectedCommentSearchOperationTypes}
-            setSelectedOperations={setSelectedCommentSearchOperationTypes}
-            colorClass="bg-gray-500"
-            triggerTitle={getOperationButtonTitle()}
-            desktopPercentageSize={50}
-          /> */}
-        </div>
+    <div className="w-full md:w-4/5">
+      <div className="bg-explorer-dark-gray text-white p-4 rounded-[6px] w-full">
         <div className="flex flex-col m-2">
           <label className="mx-2">Account name</label>
           <Input
@@ -51,7 +59,7 @@ const Comments: React.FC = () => {
             <Input
               type="number"
               value={fromBlock}
-              onChange={(e) => setFromBlock(e.target.value)}
+              onChange={(e) => setFromBlock(Number(e.target.value))}
               placeholder="1"
             />
           </div>
@@ -60,7 +68,7 @@ const Comments: React.FC = () => {
             <Input
               type="number"
               value={toBlock}
-              onChange={(e) => setToBlock(e.target.value)}
+              onChange={(e) => setToBlock(Number(e.target.value))}
               placeholder={"Headblock"}
             />
           </div>
@@ -68,13 +76,13 @@ const Comments: React.FC = () => {
         <div className="flex items-center  m-2">
           <Button
             className=" bg-blue-800 hover:bg-blue-600 rounded-[4px]"
-            onClick={() => null}
+            onClick={() => startCommentSearch()}
             disabled={!accountName?.length}
           >
             <span>Search</span>{" "}
-            {/* {loading && (
+            {commentSearch.commentSearchDataLoading && (
               <Loader2 className="animate-spin mt-1 h-4 w-4 ml-3 ..." />
-            )} */}
+            )}
           </Button>
           {false && (
             <label className="ml-2 text-muted-foreground">
@@ -83,6 +91,15 @@ const Comments: React.FC = () => {
           )}
         </div>
       </div>
+      {commentSearch.commentSearchData?.operations_result.map((foundOperation) => (
+        <DetailedOperationCard
+          className="my-6 text-white"
+          operation={foundOperation.body}
+          key={foundOperation.operation_id}
+          blockNumber={foundOperation.block_num}
+          date={foundOperation.created_at}
+        />
+      ))}
     </div>
   );
 };
