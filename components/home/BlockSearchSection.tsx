600da0edfd4ac420d39b2000fb26b906da4fdb62
jlachor <jlachor@syncad.com>
2023-12-05 12:12:24 +0100

    Add select for ranges in block search

--- components/home/BlockSearchSection.tsx
+++ components/home/BlockSearchSection.tsx
@@ -27,6 +27,25 @@ import "react-clock/dist/Clock.css";
 
 interface BlockSearchSectionProps {};
 
+const selectOptions = [
+  {
+    name: "Last blocks",
+    key: "lastBlocks"
+  },
+  {
+    name: "Last days/weeks/months",
+    key: "lastTime"
+  },
+  {
+    name: "Block range",
+    key: "blockRange"
+  },
+  {
+    name: "Time range",
+    key: "timeRange"
+  }
+]
+
 
 const BlockSearchSection: React.FC<BlockSearchSectionProps> = ({}) => {
 
@@ -52,6 +71,7 @@ const BlockSearchSection: React.FC<BlockSearchSectionProps> = ({}) => {
   const [commentPaginationPage, setCommentPaginationPage] = useState<number>(1);
   const [selectedKeys, setSelectedKeys] = useState<string[] | undefined>(undefined);
   const [lastSearchKey, setLastSearchKey] = useState<"block" | "account" | "comment" | undefined>(undefined);
+  const [rangeSelectKey, setRangeSelectKey] = useState<string>("lastBlocks");
 
   const getOperationKeys = async (
     operationTypeId: number | null
@@ -145,73 +165,103 @@ const BlockSearchSection: React.FC<BlockSearchSectionProps> = ({}) => {
   const renderRangeSection = () => {
     return (
       <>
-        <div className="flex items-center  m-2">
-          <div className="flex flex-col w-full">
-            <label className="mx-2">From block</label>
-            <Input
-              type="number"
-              value={fromBlock || ""}
-              onChange={(e) =>
-                setNumericValue(Number(e.target.value), setFromBlock)
-              }
-              placeholder="1"
-            />
-          </div>
-          <div className="flex flex-col w-full">
-            <label className="mx-2">To block</label>
-            <Input
-              type="number"
-              value={toBlock || ""}
-              onChange={(e) =>
-                setNumericValue(Number(e.target.value), setToBlock)
-              }
-              placeholder={"Headblock"}
-            />
-          </div>
-        </div>
-        <div className="flex items-center  m-2">
-          <div className="flex flex-col w-full">
-            <label className="mx-2">From date</label>
-            <DateTimePicker 
-              value={startDate} 
-              onChange={(date) => setStartDate(date!)}
-              className="text-white ml-2  border"
-              calendarClassName="text-gray-800"
-              format="yyyy/MM/dd HH:mm:ss"
-              clearIcon={null}
-              calendarIcon={null}
-              disableClock
-              showLeadingZeros={false}
-            />
-          </div>
-          <div className="flex flex-col w-full">
-            <label className="mx-2">To date</label>
-            <DateTimePicker 
-              value={endDate} 
-              onChange={(date) => setEndDate(date!)}
-              className="text-white ml-2 border"
-              calendarClassName="text-gray-800"
-              format="yyyy/MM/dd HH:mm:ss"
-              clearIcon={null}
-              calendarIcon={null}
-              disableClock
-              showLeadingZeros={false}
-            />
-          </div>
-        </div>
-        <div className="flex items-center  m-2">
-          <div className="flex flex-col w-full">
-            <label className="mx-2">Last blocks</label>
-            <Input
-              type="number"
-              value={lastBlocksValue || ""}
-              onChange={(e) =>
-                setNumericValue(Number(e.target.value), setLastBlocksValue)
-              }
-              placeholder={"Last"}
-            />
-          </div>
-        </div>
+        <Select onValueChange={setRangeSelectKey}>
+          <SelectTrigger>
+            <div>
+
+              {selectOptions.find((selectOption) => selectOption.key === rangeSelectKey)?.name}
+            </div>
+          </SelectTrigger>
+          <SelectContent className="bg-white text-black rounded-[2px] max-h-[31rem]">
+            {selectOptions.map((selectOption, index) => (
+              <SelectItem                          
+                className="m-1 text-center"
+                key={index}
+                value={selectOption.key}
+                defaultChecked={false}
+              >
+                {selectOption.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        {
+          rangeSelectKey === "blockRange" &&
+            <div className="flex items-center  m-2">
+              <div className="flex flex-col w-full">
+                <label className="mx-2">From block</label>
+                <Input
+                  type="number"
+                  value={fromBlock || ""}
+                  onChange={(e) =>
+                    setNumericValue(Number(e.target.value), setFromBlock)
+                  }
+                  placeholder="1"
+                />
+              </div>
+              <div className="flex flex-col w-full">
+                <label className="mx-2">To block</label>
+                <Input
+                  type="number"
+                  value={toBlock || ""}
+                  onChange={(e) =>
+                    setNumericValue(Number(e.target.value), setToBlock)
+                  }
+                  placeholder={"Headblock"}
+                />
+              </div>
+            </div>
+        }
+        {
+          rangeSelectKey === "lastBlocks" &&
+            <div className="flex items-center  m-2">
+              <div className="flex flex-col w-full">
+                <label className="mx-2">Last blocks</label>
+                <Input
+                  type="number"
+                  value={lastBlocksValue || ""}
+                  onChange={(e) =>
+                    setNumericValue(Number(e.target.value), setLastBlocksValue)
+                  }
+                  placeholder={"Last"}
+                />
+              </div>
+            </div>
+        }
+        {
+          rangeSelectKey === "timeRange" &&
+            <div className="flex items-center  m-2">
+              <div className="flex flex-col w-full">
+                <label className="mx-2">From date</label>
+                <DateTimePicker 
+                  value={startDate} 
+                  onChange={(date) => setStartDate(date!)}
+                  className="text-white ml-2  border"
+                  calendarClassName="text-gray-800"
+                  format="yyyy/MM/dd HH:mm:ss"
+                  clearIcon={null}
+                  calendarIcon={null}
+                  disableClock
+                  showLeadingZeros={false}
+                />
+              </div>
+              <div className="flex flex-col w-full">
+                <label className="mx-2">To date</label>
+                <DateTimePicker 
+                  value={endDate} 
+                  onChange={(date) => setEndDate(date!)}
+                  className="text-white ml-2 border"
+                  calendarClassName="text-gray-800"
+                  format="yyyy/MM/dd HH:mm:ss"
+                  clearIcon={null}
+                  calendarIcon={null}
+                  disableClock
+                  showLeadingZeros={false}
+                />
+              </div>
+            </div>
+        }
+
       </>
     )
   }
