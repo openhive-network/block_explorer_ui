fb6843f4cee6e4286b202ef9c894e58777232de3
Jakub Lachór <jlachor@syncad.com>
2023-12-14 09:35:32 +0000

    Fix manabars build errror

--- services/FetchingService.ts
+++ services/FetchingService.ts
@@ -303,19 +303,17 @@ class FetchingService {
   }
 
   async getManabars(accountName: string): Promise<Hive.Manabars | null> {
-    const chain = await createHiveChain();
     try {
+      const chain = await createHiveChain();
       const upvotePromise = await chain.calculateCurrentManabarValueForAccount(accountName, 0);
       const downvotePromise = await chain.calculateCurrentManabarValueForAccount(accountName, 1);
       const rcPromise = await chain.calculateCurrentManabarValueForAccount(accountName, 2);
       const manabars = await Promise.all([upvotePromise, downvotePromise, rcPromise]);
+      chain.delete();
       return {upvote: manabars[0], downvote: manabars[1], rc: manabars[2]};
     } catch (error) {
       console.error(error);
       return null;
-    } finally {
-      chain.delete();
-      
     }
   }
 }
