e0ab602032e9178f11724fb94b93f0c2f6369e8b
jlachor <jlachor@syncad.com>
2024-01-11 10:37:46 +0100

    Upgrade presentation of footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
   const {hafbeVersionData} = useHafbeVersion()
   const current_year = new Date().getFullYear();
   return (
-    <div className="flex flex-col justify-center items-center bg-explorer-dark-gray w-full mt-12 text-white">
-      <p className="my-2">Block Explorer by HIVE &copy; {current_year} </p>
-      <p className="my-2"><span>Last commit hash: </span>{config.gitHash}</p>
-      <p className="my-2"><span>Hafbe version hash: </span>{hafbeVersionData}</p>
+    <div className="flex flex-col justify-center items-center bg-explorer-dark-gray w-full mt-12 text-white text-sm">
+      <p className="mb-2">Block Explorer by HIVE &copy; {current_year} </p>
+      <p className="mb-2"><span>Last commit hash: </span>{config.gitHash}</p>
+      <p className="mb-2"><span>Hafbe version hash: </span>{hafbeVersionData}</p>
     </div>
   );
 }
