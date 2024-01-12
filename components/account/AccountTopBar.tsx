200123fc8369c00193e9a30a7872889aea31511e
Piotr Berezka <berezkapiotr@gmail.com>
2024-01-12 13:15:04 +0100

    account page top bar and fixes

--- components/account/AccountTopBar.tsx
+++ components/account/AccountTopBar.tsx
@@ -1,14 +1,28 @@
+import Hive from "@/types/Hive";
 import CustomPagination from "../CustomPagination";
 import JumpToPage from "../JumpToPage";
-import OperationTypesDialog from "../OperationTypesDialog";
 import ScrollTopButton from "../ScrollTopButton";
 import { config } from "@/Config";
+import OperationTypesDialog from "../OperationTypesDialog";
+import { getOperationButtonTitle } from "@/utils/UI";
 
-const AccountPagination = ({
+interface AccountTopBarProps {
+  page: number;
+  setPage: (page: number) => void;
+  accountOperations: Hive.OperationsCount;
+  accountOperationTypes: Hive.OperationPattern[];
+  onOperationsSelect: (filters: number[]) => void;
+  selectedFilters: number[];
+}
+
+const AccountTopBar: React.FC<AccountTopBarProps> = ({
   page,
   setPage,
   accountOperations,
-}: any) => {
+  accountOperationTypes,
+  onOperationsSelect,
+  selectedFilters
+}) => {
   return (
     <div className="bg-explorer-orange items-center grid grid-flow-row-dense grid-cols-3 top-14 md:top-16 right-0 left-0 p-2 z-10">
       <div className="col-span-3 md:col-span-2 md:justify-self-end justify-self-center z-20 max-w-full">
@@ -22,7 +36,7 @@ const AccountPagination = ({
       </div>
 
       <div className="justify-self-end col-span-4 md:col-span-1">
-        <div className="grid gap-x-3 grid-flow-row-dense grid-cols-3">
+        <div className="grid gap-x-3 grid-flow-row-dense grid-cols-6">
           <JumpToPage
             currentPage={page}
             onPageChange={(page: number) => setPage(page)}
@@ -30,10 +44,17 @@ const AccountPagination = ({
           <div className="justify-self-end self-center col-span-2">
             <ScrollTopButton />
           </div>
+          <OperationTypesDialog
+            operationTypes={accountOperationTypes}
+            setSelectedOperations={onOperationsSelect}
+            selectedOperations={selectedFilters}
+            buttonClassName="bg-explorer-dark-gray col-span-3 justify-self-end col-start-4"
+            triggerTitle={getOperationButtonTitle(selectedFilters, accountOperationTypes)}
+          />
         </div>
       </div>
     </div>
   );
 };
 
-export default AccountPagination;
+export default AccountTopBar;
--- utils/UI.ts
+++ utils/UI.ts
@@ -5,9 +5,12 @@ export const scrollTo = (ref: MutableRefObject<any>) => {
   ref.current?.scrollIntoView();
 };
 
-export const getOperationButtonTitle = (selectedOperationTypes: number[], operationsTypes?: Hive.OperationPattern[]): string => {
+export const getOperationButtonTitle = (
+  selectedOperationTypes: number[],
+  operationsTypes?: Hive.OperationPattern[]
+): string => {
   if (selectedOperationTypes && selectedOperationTypes.length === 1)
-    return operationsTypes?.[selectedOperationTypes[0]].operation_name || "";
+    return operationsTypes?.[selectedOperationTypes[0]]?.operation_name || "";
   if (selectedOperationTypes && selectedOperationTypes.length > 1)
     return `${selectedOperationTypes.length} operation types`;
   return "Operation types";
