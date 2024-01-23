efb2f5d12086d101123a86ca0091cc149252a025
Piotr Berezka <berezkapiotr@gmail.com>
2024-01-23 10:07:22 +0100

    refactor search section and its subcomponents

--- components/home/searches/CommentsSearch.tsx
+++ components/home/searches/CommentsSearch.tsx
@@ -1,8 +1,3 @@
-import {
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import { Input } from "@/components/ui/input";
 import SearchRanges from "@/components/searchRanges/SearchRanges";
 import OperationTypesDialog from "@/components/OperationTypesDialog";
@@ -37,7 +32,7 @@ const CommentsSearch: React.FC<CommentsSearchProps> = ({
 
   const searchRanges = useSearchRanges();
   const { getRangesValues } = searchRanges;
-  
+
   const onButtonClick = async () => {
     if (accountName !== "") {
       const {
@@ -46,7 +41,7 @@ const CommentsSearch: React.FC<CommentsSearchProps> = ({
         payloadStartDate,
         payloadEndDate,
       } = await getRangesValues();
-  
+
       const commentSearchProps: Explorer.CommentSearchProps = {
         accountName,
         permlink: permlink !== "" ? permlink : undefined,
@@ -62,71 +57,61 @@ const CommentsSearch: React.FC<CommentsSearchProps> = ({
     }
   };
 
-
   return (
-    <AccordionItem value="comment">
-      <AccordionTrigger>Comment search</AccordionTrigger>
-      <AccordionContent className="px-2 flex flex-col gap-y-4">
-        <p className="ml-2">
-          Find all operations related to comments of given account or for exact
-          permlink.
-        </p>
-        <div className="flex flex-col">
-          <label className="ml-2">Account name *</label>
-          <Input
-            className="w-1/2 md:w-1/3 bg-gray-700"
-            type="text"
-            value={accountName || ""}
-            onChange={(e) =>
-              setAccountName(e.target.value)
-            }
-            placeholder="---"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label className="ml-2">Permlink</label>
-          <Input
-            className="w-full bg-gray-700"
-            type="text"
-            value={permlink}
-            onChange={(e) =>
-              setPermlink(e.target.value)
-            }
-            placeholder="---"
-          />
-        </div>
-        <SearchRanges rangesProps={searchRanges} safeTimeRangeDisplay/>
-        <div className="flex items-center">
-          <OperationTypesDialog
-            operationTypes={operationsTypes?.filter((opType) =>
-              config.commentOperationsTypeIds.includes(opType.op_type_id)
-            )}
-            selectedOperations={selectedCommentSearchOperationTypes}
-            setSelectedOperations={setSelectedCommentSearchOperationTypes}
-            buttonClassName="bg-gray-500"
-            triggerTitle={getOperationButtonTitle(
-              selectedCommentSearchOperationTypes,
-              operationsTypes
-            )}
-          />
-        </div>
-        <div className="flex items-center">
-          <Button
-            className=" bg-blue-800 hover:bg-blue-600 rounded"
-            onClick={onButtonClick}
-            disabled={!accountName}
-          >
-            <span>Search</span>{" "}
-            {loading && (
-              <Loader2 className="animate-spin h-4 w-4  ..." />
-            )}
-          </Button>
-          {!accountName && (
-            <label className=" text-muted-foreground">Set account name</label>
+    <>
+      <p className="ml-2">
+        Find all operations related to comments of given account or for exact
+        permlink.
+      </p>
+      <div className="flex flex-col">
+        <label className="ml-2">Account name *</label>
+        <Input
+          className="w-1/2 md:w-1/3 bg-gray-700"
+          type="text"
+          value={accountName || ""}
+          onChange={(e) => setAccountName(e.target.value)}
+          placeholder="---"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label className="ml-2">Permlink</label>
+        <Input
+          className="w-full bg-gray-700"
+          type="text"
+          value={permlink}
+          onChange={(e) => setPermlink(e.target.value)}
+          placeholder="---"
+        />
+      </div>
+      <SearchRanges rangesProps={searchRanges} safeTimeRangeDisplay />
+      <div className="flex items-center">
+        <OperationTypesDialog
+          operationTypes={operationsTypes?.filter((opType) =>
+            config.commentOperationsTypeIds.includes(opType.op_type_id)
           )}
-        </div>
-      </AccordionContent>
-    </AccordionItem>
+          selectedOperations={selectedCommentSearchOperationTypes}
+          setSelectedOperations={setSelectedCommentSearchOperationTypes}
+          buttonClassName="bg-gray-500"
+          triggerTitle={getOperationButtonTitle(
+            selectedCommentSearchOperationTypes,
+            operationsTypes
+          )}
+        />
+      </div>
+      <div className="flex items-center">
+        <Button
+          className=" bg-blue-800 hover:bg-blue-600 rounded"
+          onClick={onButtonClick}
+          disabled={!accountName}
+        >
+          <span>Search</span>{" "}
+          {loading && <Loader2 className="animate-spin h-4 w-4  ..." />}
+        </Button>
+        {!accountName && (
+          <label className=" text-muted-foreground">Set account name</label>
+        )}
+      </div>
+    </>
   );
 };
 
