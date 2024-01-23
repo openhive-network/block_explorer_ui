728fcf6be8c8f8ce7dfc6a03293494099601f48e
jlachor <jlachor@syncad.com>
2024-01-23 14:21:01 +0100

    Update version of wax, adjust

--- lib/Formatter.ts
+++ lib/Formatter.ts
@@ -1,15 +1,15 @@
 import { config } from "@/Config";
 import Hive from "@/types/Hive";
-import { WaxFormattable } from "@hive/wax/web";
+import { IFormatFunctionArguments, WaxFormattable } from "@hive/wax/web";
 import moment from "moment";
 
 
 class TestFormatter {
     // Example formatter
     @WaxFormattable()
-    time(value: Hive.DynamicGlobalBlock) {
-      const time = moment(value.time).format(config.baseMomentTimeFormat);
-      return {...value, time};
+    time({ source }: IFormatFunctionArguments<Hive.DynamicGlobalBlock>) {
+      const time = moment(source.time).format(config.baseMomentTimeFormat);
+      return {...structuredClone(source), time};
     }
   
   }
