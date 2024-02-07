13bfc7acc235ef75eeba82bd99f514e11666aff4
jlachor <jlachor@syncad.com>
2024-02-07 13:30:49 +0100

    Change useHeadBlockNum for two methods

--- services/FetchingService.ts
+++ services/FetchingService.ts
@@ -299,6 +299,11 @@ class FetchingService {
     return await this.callApi("get_op_count_in_block", requestBody);
   }
 
+  async getHafbeLastSyncedBlock():Promise<number> {
+    const requestBody = {};
+    return await this.callApi("get_hafbe_last_synced_block", requestBody);
+  }
+
   async getManabars(accountName: string, hiveChain: IHiveChainInterface): Promise<Hive.Manabars | null> {
     try {
       const upvotePromise = hiveChain.calculateCurrentManabarValueForAccount(accountName, 0);
