97d7ad930cba980e61118e1fd3b64a620d3bd4f5
Piotr Berezka <berezkapiotr@gmail.com>
2024-02-12 10:00:40 +0100

    fix set page on data change

--- api/accountPage/useAccountOperations.ts
+++ api/accountPage/useAccountOperations.ts
@@ -2,7 +2,7 @@ import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import fetchingService from "@/services/FetchingService";
 import Hive from "@/types/Hive";
 import Explorer from "@/types/Explorer";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const useAccountOperations = (
   accountOperationsProps?: Explorer.AccountSearchOperationsProps,
@@ -13,7 +13,6 @@ const useAccountOperations = (
     data: accountOperations,
     isFetching: isAccountOperationsLoading,
     isError: isAccountOperationsError,
-    refetch
   }: UseQueryResult<Hive.AccountOperationsResponse> = useQuery({
     queryKey: ["account_operations", accountOperationsProps],
     queryFn: () => fetchAccountOperations(accountOperationsProps),
@@ -31,12 +30,6 @@ const useAccountOperations = (
     if (!accountOperationsProps) return null;
     return await fetchingService.getOpsByAccount(accountOperationsProps);
   };
-
-  useEffect(() => {
-    if(liveUpdate) {
-      refetch();
-    }
-  }, [liveUpdate, refetch]);
 
   return {
     accountOperations,
