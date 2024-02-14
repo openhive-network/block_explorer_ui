88b22a1542df9da807f5cdb07cfd3a5aaef993fb
Piotr Berezka <berezkapiotr@gmail.com>
2024-02-14 11:13:39 +0100

    update comments routing scheme

--- pages/comments.tsx
+++ pages/comments.tsx
@@ -8,6 +8,9 @@ import useOperationTypes from "@/api/common/useOperationsTypes";
 import JumpToPage from "@/components/JumpToPage";
 import { useURLParams } from "@/utils/Hooks";
 import CommentsSearch from "@/components/home/searches/CommentsSearch";
+import { useUserSettingsContext } from "@/components/contexts/UserSettingsContext";
+import JSONView from "@/components/JSONView";
+import { useRouter } from "next/router";
 
 const defaultSearchParams: Explorer.CommentSearchParams = {
   accountName: undefined,
@@ -33,17 +36,26 @@ const Comments: React.FC = () => {
     Explorer.CommentSearchProps | undefined
   >(undefined);
 
+  const router = useRouter();
+
+  const permlinkFromRoute = router.query.permlink;
+
   const commentSearch = useCommentSearch(commentSearchProps);
-  const { paramsState, setParams } = useURLParams(defaultSearchParams);
+  const { paramsState, setParams } = useURLParams(
+    defaultSearchParams,
+    permlinkFromRoute
+      ? [{ key: "accountName", prefix: "@" }, { key: "permlink" }]
+      : [{ path: "comments" }, { key: "accountName", prefix: "@" }]
+  );
 
   const operationsTypes =
-  useOperationTypes().operationsTypes?.filter((operation) =>
-    config.commentOperationsTypeIds.includes(operation.op_type_id)
-  ) || [];
+    useOperationTypes().operationsTypes?.filter((operation) =>
+      config.commentOperationsTypeIds.includes(operation.op_type_id)
+    ) || [];
 
   const startCommentSearch = async (props: Explorer.CommentSearchParams) => {
     if (!!props.accountName) {
-      props.accountName = props.accountName.replace("@", "");
+      props = formatAccountName(props);
       setCommentSearchProps(props as Explorer.CommentSearchProps);
       setPreviousCommentSearchProps(props as Explorer.CommentSearchProps);
       setParams({ ...paramsState, ...props });
@@ -62,6 +74,11 @@ const Comments: React.FC = () => {
     }
   };
 
+  const formatAccountName = (params: Explorer.CommentSearchParams) => {
+    params.accountName = params.accountName?.replace("@", "");
+    return params;
+  };
+
   useEffect(() => {
     if (paramsState && !initialSearch) {
       startCommentSearch(paramsState);
@@ -75,7 +92,7 @@ const Comments: React.FC = () => {
         <CommentsSearch
           startCommentsSearch={startCommentSearch}
           operationsTypes={operationsTypes}
-          data={paramsState}
+          data={formatAccountName(paramsState)}
           loading={commentSearch.commentSearchDataLoading}
         />
       </div>
