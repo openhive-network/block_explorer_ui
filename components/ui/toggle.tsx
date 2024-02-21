bd73f1e0c1ef0ea9cde121888912abaf63e71540
jlachor <jlachor@syncad.com>
2024-02-21 10:42:47 +0100

    Fix toggle warn

--- components/ui/toggle.tsx
+++ components/ui/toggle.tsx
@@ -9,35 +9,42 @@ interface ToggleProps {
   onClick: () => void;
 }
 
-const Toggle = React.forwardRef<ToggleProps, ToggleProps>(
-  ({ leftLabel, rightLabel, checked, className = "", onClick }) => {
-    return (
-      <div className={cn("flex gap-x-2 items-center", className)} data-testid="toggle">
-        {leftLabel && <p>{leftLabel}</p>}
+const Toggle: React.FC<ToggleProps> = ({
+  leftLabel,
+  rightLabel,
+  checked,
+  className = "",
+  onClick,
+}) => {
+  return (
+    <div
+      className={cn("flex gap-x-2 items-center", className)}
+      data-testid="toggle"
+    >
+      {leftLabel && <p>{leftLabel}</p>}
+      <div
+        className={cn(
+          "w-10 h-5 cursor-pointer rounded-3xl border-2 border-white relative",
+          {
+            "bg-green-600": checked,
+            "bg-transparent": !checked,
+          }
+        )}
+        onClick={onClick}
+      >
         <div
           className={cn(
-            "w-10 h-5 cursor-pointer rounded-3xl border-2 border-white relative",
+            "w-3.5 h-3.5 bg-white rounded-full absolute top-px left-px transition duration-300 ease-in-out",
             {
-              "bg-green-600": checked,
-              "bg-transparent": !checked,
+              "translate-x-[20px]": checked,
             }
           )}
-          onClick={onClick}
-        >
-          <div
-            className={cn(
-              "w-3.5 h-3.5 bg-white rounded-full absolute top-px left-px transition duration-300 ease-in-out",
-              {
-                "translate-x-[20px]": checked,
-              }
-            )}
-          ></div>
-        </div>
-        {rightLabel && <p>{rightLabel}</p>}
+        ></div>
       </div>
-    );
-  }
-);
+      {rightLabel && <p>{rightLabel}</p>}
+    </div>
+  );
+};
 
 Toggle.displayName = "Toggle";
 
