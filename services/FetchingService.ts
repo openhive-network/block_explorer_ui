c6ee0330c7be62833a5c4974e2348c2ed5bc066e
jlachor <jlachor@syncad.com>
2024-02-27 08:33:14 +0100

    Change condition for wrong requests

--- services/FetchingService.ts
+++ services/FetchingService.ts
@@ -25,11 +25,7 @@ class FetchingService {
         body: JSON.stringify(requestBody),
       });
       const jsonResponse = await response.json();
-      if (
-        typeof jsonResponse === "object" &&
-        Object.keys(jsonResponse).length === 0 &&
-        !Array.isArray(jsonResponse)
-      )
+      if (!response.ok)
         throw new Error("No data from API");
       return jsonResponse
     } catch (error) {
