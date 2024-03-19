e9100801a75a6e52dbb96f0e0f7818f57a93667c
adubel <adubel@syncad.com>
2024-03-19 09:51:38 +0100

    add first test for transaction page

---
+++ tests/playwright/e2e/transactionPage.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from "@playwright/test";
+import { MainPage } from "../support/pages/mainPage";
+import { BlockPage } from "../support/pages/blockPage";
+import { AccountPage } from "../support/pages/accountPage";
+import { TransactionPage } from "../support/pages/transactionPage"
+
+test.describe('Transaction page - tests', () => {
+    let mainPage: MainPage;
+    let blockPage: BlockPage;
+    let accountPage: AccountPage;
+    let transactionPage: TransactionPage;
+
+    test.beforeEach(async ({ page }) => {
+        mainPage = new MainPage(page);
+        blockPage = new BlockPage(page);
+        accountPage = new AccountPage(page);
+        transactionPage = new TransactionPage(page);
+
+        await mainPage.gotoBlockExplorerPage();
+    });
+
+    test('Validate that transaction details are displayed correctly', async ({page}) =>{
+        await mainPage.headBlockCardBlockLink.click()
+        await page.locator('[href*="transaction"]').first().click()
+        await expect(transactionPage.transactionHeader).toBeVisible()
+        await expect(transactionPage.transactionHeaderBlockNumber).toBeVisible()
+        await expect(transactionPage.transactionHeaderDate).toBeVisible()
+        await expect(transactionPage.transactionHeaderHash).toBeVisible()
+        await expect(transactionPage.transactionHeaderHashBlock).toBeVisible()
+        await expect(transactionPage.transactionDetails).toBeVisible()
+    })
+});
