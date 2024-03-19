9f6626ad976359864d7a0f4beaa53c7d6e390465
techcoderx <engtianxi@outlook.com>
2024-03-19 18:26:32 +0800

    fix dark mode block property dropdown

--- components/home/searches/BlockSearch.tsx
+++ components/home/searches/BlockSearch.tsx
@@ -174,10 +174,10 @@ const BlockSearch: React.FC<BlockSearchProps> = ({
                 </div>
               )}
             </SelectTrigger>
-            <SelectContent className="bg-white text-black rounded-sm max-h-[31rem] overflow-y-scroll">
+            <SelectContent className="bg-white text-black dark:bg-explorer-dark-gray dark:text-white rounded-sm max-h-[31rem] overflow-y-scroll">
               {operationKeysData?.map((keys, index) => (
                 <SelectItem
-                  className="m-1 text-center"
+                  className="m-1 text-center hover:bg-explorer-dark-gray hover:text-white hover:dark:bg-white hover:dark:text-black hover:cursor-pointer"
                   key={index}
                   value={index.toFixed(0)}
                   defaultChecked={false}
