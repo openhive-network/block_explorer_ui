46f24c6f34e0ae3f1263a577d33381a1586da141
jlachor <jlachor@syncad.com>
2024-03-26 13:45:55 +0100

    Mark formatted times with UTC

--- Config.ts
+++ Config.ts
@@ -3,7 +3,7 @@ import env from "@beam-australia/react-env"
 export const config = {
   nodeAddress: `${env('HIVE_BLOG_API_ADDRESS') ? env('HIVE_BLOG_API_ADDRESS') : 'https://api.hive.blog'}`,
   apiAddress: `${env('API_ADDRESS') ? env('API_ADDRESS') : 'https://hafbe.openhive.network/rpc'}`,
-  baseMomentTimeFormat: "YYYY/MM/DD hh:mm:ss",
+  baseMomentTimeFormat: "YYYY/MM/DD hh:mm:ss UTC",
   gitHash: process.env.NEXT_PUBLIC_COMMIT_HASH,
   opsBodyLimit: 100000,
   commentOperationsTypeIds: [0, 1, 17, 19, 51, 53, 61, 63, 72, 73],
