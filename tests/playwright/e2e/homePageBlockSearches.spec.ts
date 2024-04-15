cc147e381908292a3c7423e49d834ae5688ac111
adubel <adubel@syncad.com>
2024-04-15 10:20:07 +0200

    fix test check url

--- tests/playwright/e2e/homePageBlockSearches.spec.ts
+++ tests/playwright/e2e/homePageBlockSearches.spec.ts
@@ -267,7 +267,7 @@ test.describe('Home page - searches', () => {
     test("Check if url is correct and contain blocku number and user name", async ({ page }) => {
 
         const userName = 'gtg'
-        
+
         await mainPage.accountNameInput.fill(userName)
         await mainPage.blockSearchBtn.click()
         await expect(mainPage.firstResultBlock).toBeVisible()
@@ -278,9 +278,7 @@ test.describe('Home page - searches', () => {
         await expect(blockPage.producedData).toBeVisible()
         
         const url = await page.url()
-        // await expect(url).toContain(firstBlockNumber)
-        // await expect(url).toContain(`accountName=${userName}`)
-
-        await expect(url).toEqual(`http://localhost:5000/block/${firstBlockNumber}?accountName=${userName}`)
+        await expect(url).toContain(firstBlockNumber)
+        await expect(url).toContain(`accountName=${userName}`)
     })
 });
