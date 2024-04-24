466bfdcff98b840b3cd5055cf1613a8393e1c498
Lukas <lukas.budginas@gmail.com>
2024-04-24 17:17:30 +0300

    Update search ranges style

--- components/searchRanges/SearchRanges.tsx
+++ components/searchRanges/SearchRanges.tsx
@@ -46,8 +46,11 @@ const SearchRanges: React.FC<SearchRangesProps> = ({
 
   return (
     <div className="border-y border-solid border-gray-600 py-2 flex flex-col gap-y-2">
-      <Select onValueChange={setRangeSelectKey} value={rangeSelectKey}>
-        <SelectTrigger className="bg-gray-700">
+      <Select
+        onValueChange={setRangeSelectKey}
+        value={rangeSelectKey}
+      >
+        <SelectTrigger className="w-1/2 border-0 border-b-2 bg-explorer-dark-gray ">
           {
             rangeSelectOptions.find(
               (selectOption) => selectOption.key === rangeSelectKey
@@ -72,13 +75,14 @@ const SearchRanges: React.FC<SearchRangesProps> = ({
         <div className="flex items-center">
           <div className="flex flex-col w-full">
             <Input
-              className="bg-gray-700"
+              className="w-1/2 border-0 border-b-2 bg-explorer-dark-gray"
               type="number"
               value={lastBlocksValue || ""}
               onChange={(e) =>
                 setNumericValue(Number(e.target.value), setLastBlocksValue)
               }
               placeholder={"Last"}
+              min="0"
             />
           </div>
         </div>
@@ -86,26 +90,30 @@ const SearchRanges: React.FC<SearchRangesProps> = ({
       {rangeSelectKey === "lastTime" && (
         <>
           <div className="flex items-center justify-center">
-            <div className="flex flex-col w-full ">
+            <div className="flex flex-col w-full mr-2">
               <Input
                 type="number"
-                className="bg-gray-700"
+                className="bg-explorer-dark-gray border-0 border-b-2 text-white"
                 value={lastTimeUnitValue || ""}
                 onChange={(e) =>
                   setNumericValue(Number(e.target.value), setLastTimeUnitValue)
                 }
                 placeholder={"Last"}
+                min="0"
               />
             </div>
             <Select onValueChange={setTimeUnitSelectKey}>
-              <SelectTrigger className="bg-gray-700">
+              <SelectTrigger className="pl-2 bg-explorer-dark-gray border-0 border-b-2 text-white">
                 {
                   timeSelectOptions.find(
                     (selectOption) => selectOption.key === timeUnitSelectKey
                   )?.name
                 }
               </SelectTrigger>
-              <SelectContent className="bg-white text-black rounded-sm max-h-[31rem] dark:bg-explorer-dark-gray dark:text-white" data-testid="select-time-option-units">
+              <SelectContent
+                className="bg-white text-black rounded-sm max-h-[31rem] dark:bg-explorer-dark-gray dark:text-white"
+                data-testid="select-time-option-units"
+              >
                 {timeSelectOptions.map((selectOption, index) => (
                   <SelectItem
                     className="text-center hover:dark:bg-white hover:dark:text-black"
@@ -123,47 +131,61 @@ const SearchRanges: React.FC<SearchRangesProps> = ({
       )}
       {rangeSelectKey === "blockRange" && (
         <div className="flex items-center">
-          <div className="flex flex-col w-full">
-            <label className="ml-2">From block</label>
+          <div className="flex flex-col w-full mr-2">
+            {/* <label className="ml-2">From block</label> */}
             <Input
               type="number"
-              className="bg-gray-700"
+              className="bg-explorer-dark-gray border-0 border-b-2"
               data-testid="from-block-input"
               value={fromBlock || ""}
               onChange={(e) =>
                 setNumericValue(Number(e.target.value), setFromBlock)
               }
-              placeholder="1"
+              placeholder="From"
+              min="0"
             />
           </div>
           <div className="flex flex-col w-full">
-            <label className="ml-2">To block</label>
+            {/* <label className="ml-2">To block</label> */}
             <Input
-              className="bg-gray-700"
+              className="bg-explorer-dark-gray border-0 border-b-2"
               data-testid="headblock-number"
               type="number"
               value={toBlock || ""}
               onChange={(e) =>
                 setNumericValue(Number(e.target.value), setToBlock)
               }
-              placeholder={"Headblock"}
+              placeholder={"To"}
+              min={0}
             />
           </div>
         </div>
       )}
       {rangeSelectKey === "timeRange" && (
-        <div className="flex items-center flex-wrap">
-          <div className="flex flex-col w-full">
-            <label className="ml-2">From date</label>
+        <div className="flex items-center flex-wrap mt-5">
+          <div className="flex w-full justify-around">
+            <div className="flex-col w-full mr-2">
+              <label className="ml-2 my-2">From date</label>
+              <DateTimePicker
+                date={startDate || new Date()}
+                setDate={setStartDate}
+              />
+            </div>
+            <div className="flex-col w-full">
+              <label className="ml-2 my-2">To date</label>
+              <DateTimePicker
+                date={endDate || new Date()}
+                setDate={setEndDate}
+              />
+            </div>
+          </div>
+          {/* <div className="flex flex-col w-1/2">
+            <label className="ml-2 my-2">To date</label>
             <DateTimePicker
-              date={startDate || new Date()}
-              setDate={setStartDate}
+              date={endDate || new Date()}
+              setDate={setEndDate}
             />
-          </div>
-          <div className="flex flex-col w-full">
-            <label className="ml-2">To date</label>
-            <DateTimePicker date={endDate || new Date()} setDate={setEndDate} />
-          </div>
+          </div> */}
         </div>
       )}
     </div>
