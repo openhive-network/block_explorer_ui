38c60e5f689077f0dd0247e1978b4782d27eed2e
jlachor <jlachor@syncad.com>
2024-06-03 12:35:17 +0200

    Fix useAccountDetail selector

--- api/accountPage/useAccountDetails.ts
+++ api/accountPage/useAccountDetails.ts
@@ -3,43 +3,7 @@ import fetchingService from "@/services/FetchingService";
 import Hive from "@/types/Hive";
 import Explorer from "@/types/Explorer";
 import { useHiveChainContext } from "@/components/contexts/HiveChainContext";
-import { IHiveChainInterface } from "@hive/wax";
 import { formatAndDelocalizeTime } from "@/utils/TimeUtils";
-
-const accountDetailsSelector = (
-  data: Hive.AccountDetailsQueryResponse,
-  hiveChain: IHiveChainInterface | undefined
-): Explorer.FormattedAccountDetails => {
-  const accountDetails = {
-    ...data,
-    balance: hiveChain?.hive(data.balance),
-    savings_balance: hiveChain?.hive(data.savings_balance),
-    hbd_balance: hiveChain?.hbd(data.hbd_balance),
-    hbd_saving_balance: hiveChain?.hbd(data.hbd_saving_balance),
-    reward_hbd_balance: hiveChain?.hbd(data.reward_hbd_balance),
-    reward_vesting_balance: hiveChain?.vests(data.reward_vesting_balance),
-    reward_vesting_hive: hiveChain?.vests(data.reward_vesting_hive),
-    reward_hive_balance: hiveChain?.hive(data.reward_hive_balance),
-    vesting_withdraw_rate: hiveChain?.vests(data.vesting_withdraw_rate),
-    vesting_shares: hiveChain?.vests(data.vesting_shares),
-    delegated_vesting_shares: hiveChain?.vests(data.delegated_vesting_shares),
-    received_vesting_shares: hiveChain?.vests(data.received_vesting_shares),
-    post_voting_power: hiveChain?.vests(data.post_voting_power),
-    posting_rewards: hiveChain?.vests(data.posting_rewards),
-    curation_rewards: hiveChain?.vests(data.curation_rewards),
-    vesting_balance: hiveChain?.vests(data.vesting_balance),
-    last_account_recovery: formatAndDelocalizeTime(data.last_account_recovery),
-    created: formatAndDelocalizeTime(data.created),
-    last_vote_time: formatAndDelocalizeTime(data.last_vote_time),
-  };
-  const formattedAccountDetails = hiveChain?.formatter.format(
-    accountDetails
-  ) as Explorer.FormattedAccountDetails;
-  delete formattedAccountDetails.last_post;
-  delete formattedAccountDetails.last_root_post;
-  delete formattedAccountDetails.post_count;
-  return formattedAccountDetails;
-};
 
 const useAccountDetails = (accountName: string) => {
   const { hiveChain } = useHiveChainContext();
@@ -52,7 +16,7 @@ const useAccountDetails = (accountName: string) => {
     queryKey: ["account_details", accountName],
     queryFn: () => fetchingService.getAccount(accountName),
     refetchOnWindowFocus: false,
-    select: (data) => accountDetailsSelector(data, hiveChain),
+    select: (data) => accountDetailsSelector(data),
     enabled: !!accountName && !!accountName.length,
   });
 
