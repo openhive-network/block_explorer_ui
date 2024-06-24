c898efcc9021c789ef1e5021bb6fdd83df234436
jlachor <jlachor@syncad.com>
2024-06-24 10:19:05 +0200

    Modify healthcheckers

--- components/HealthChecker.tsx
+++ components/HealthChecker.tsx
@@ -1,8 +1,9 @@
 import { IHiveChainInterface } from "@hiveio/wax";
 import { HealthChecker } from "@hiveio/wax";
+import { useEffect } from "react";
 
 interface HealthCheckerComponentProps {
-  hiveChain: IHiveChainInterface
+  hiveChain?: IHiveChainInterface
 }
 
 const HealthCheckerComponent: React.FC<HealthCheckerComponentProps> = ({
@@ -10,6 +11,14 @@ const HealthCheckerComponent: React.FC<HealthCheckerComponentProps> = ({
 }) => {
 
   const hc = new HealthChecker();
+
+  console.log('HC', hc);
+
+  useEffect(() => { 
+    if (hc && hiveChain) {
+      hc.register(hiveChain?.api.block_api.get_block, {block_num: 1}, data => data.block?.previous === "0000000000000000000000000000000000000000", ["api.openhive.network"])
+    }
+  }, [hc, hiveChain])
 
   return (
     <div>
