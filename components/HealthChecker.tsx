66012edc1a1bcb6808468a32053c64102d0bde5a
jlachor <jlachor@syncad.com>
2024-06-25 10:24:21 +0200

    Update initialization, change api list

--- components/HealthChecker.tsx
+++ components/HealthChecker.tsx
@@ -1,6 +1,6 @@
 import { IHiveChainInterface, IScoredEndpoint } from "@hiveio/wax";
 import { HealthChecker } from "@hiveio/wax";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface HealthCheckerComponentProps {
   hiveChain?: IHiveChainInterface
@@ -10,31 +10,33 @@ const HealthCheckerComponent: React.FC<HealthCheckerComponentProps> = ({
   hiveChain
 }) => {
 
+  const [chainInitialized, setChainIntialized] = useState<boolean>(false);
+
   const apiList = [
-    "api.hive.blog",
-    "api.openhive.network",
-    "anyx.io",
-    "rpc.ausbit.dev",
-    "rpc.mahdiyari.info",
-    "api.hive.blue",
-    "techcoderx.com",
-    "hive.roelandp.nl",
-    "hived.emre.sh",
-    "api.deathwing.me",
-    "api.c0ff33a.uk",
-    "hive-api.arcange.eu",
-    "hive-api.3speak.tv",
-    "hiveapi.actifit.io"
+    "https://api.hive.blog",
+    "https://api.openhive.network",
+    "https://anyx.io",
+    "https://rpc.ausbit.dev",
+    "https://rpc.mahdiyari.info",
+    "https://api.hive.blue",
+    "https://techcoderx.com",
+    "https://hive.roelandp.nl",
+    "https://hived.emre.sh",
+    "https://api.deathwing.me",
+    "https://api.c0ff33a.uk",
+    "https://hive-api.arcange.eu",
+    "https://hive-api.3speak.tv",
+    "https://hiveapi.actifit.io"
   ];
 
   const hc = new HealthChecker();
 
-  console.log('HC', hc);
 
   useEffect(() => { 
-    if (hc && hiveChain) {
+    if (hc && hiveChain && !chainInitialized) {
       hc.register(hiveChain?.api.block_api.get_block, {block_num: 1}, data => data.block?.previous === "0000000000000000000000000000000000000000", apiList);
       hc.on("data", (data: Array<IScoredEndpoint>) => { console.log(JSON.stringify(data)); });
+      setChainIntialized(true);
     }
   }, [hiveChain])
 
