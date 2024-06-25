dd9e1adb0e050999c945c7834b206e87c390495d
jlachor <jlachor@syncad.com>
2024-06-25 12:01:58 +0200

    Show public hive apis in healthchecker

--- components/HealthChecker.tsx
+++ components/HealthChecker.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { IHiveChainInterface, IScoredEndpoint } from "@hiveio/wax";
 import { HealthChecker } from "@hiveio/wax";
 import { useEffect, useState } from "react";
@@ -11,6 +12,8 @@ const HealthCheckerComponent: React.FC<HealthCheckerComponentProps> = ({
 }) => {
 
   const [chainInitialized, setChainIntialized] = useState<boolean>(false);
+
+  const [scoredEndpoints, setScoredEndpoints] = useState<IScoredEndpoint[]>([]);
 
   const apiList = [
     "https://api.hive.blog",
@@ -35,14 +38,18 @@ const HealthCheckerComponent: React.FC<HealthCheckerComponentProps> = ({
   useEffect(() => { 
     if (hc && hiveChain && !chainInitialized) {
       hc.register(hiveChain?.api.block_api.get_block, {block_num: 1}, data => data.block?.previous === "0000000000000000000000000000000000000000", apiList);
-      hc.on("data", (data: Array<IScoredEndpoint>) => { console.log(JSON.stringify(data)); });
+      hc.on("data", (data: Array<IScoredEndpoint>) => { console.log(JSON.stringify(data)); setScoredEndpoints(data) });
       setChainIntialized(true);
     }
   }, [hiveChain])
 
   return (
     <div>
-
+      {scoredEndpoints.map((scoredEndpoint) => (
+        <div className={cn({"text-red-800": scoredEndpoint.score <= 0})}>
+          {scoredEndpoint.endpointUrl}
+        </div>
+      ))}
     </div>
   );
 };
