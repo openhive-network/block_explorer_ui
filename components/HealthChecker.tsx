76fba773b1a63f78920680e6c45a0653c76548a1
jlachor <jlachor@syncad.com>
2024-06-25 09:53:24 +0200

    Add api lists and onUpdate method

--- components/HealthChecker.tsx
+++ components/HealthChecker.tsx
@@ -1,4 +1,4 @@
-import { IHiveChainInterface } from "@hiveio/wax";
+import { IHiveChainInterface, IScoredEndpoint } from "@hiveio/wax";
 import { HealthChecker } from "@hiveio/wax";
 import { useEffect } from "react";
 
@@ -10,15 +10,33 @@ const HealthCheckerComponent: React.FC<HealthCheckerComponentProps> = ({
   hiveChain
 }) => {
 
+  const apiList = [
+    "api.hive.blog",
+    "api.openhive.network",
+    "anyx.io",
+    "rpc.ausbit.dev",
+    "rpc.mahdiyari.info",
+    "api.hive.blue",
+    "techcoderx.com",
+    "hive.roelandp.nl",
+    "hived.emre.sh",
+    "api.deathwing.me",
+    "api.c0ff33a.uk",
+    "hive-api.arcange.eu",
+    "hive-api.3speak.tv",
+    "hiveapi.actifit.io"
+  ];
+
   const hc = new HealthChecker();
 
   console.log('HC', hc);
 
   useEffect(() => { 
     if (hc && hiveChain) {
-      hc.register(hiveChain?.api.block_api.get_block, {block_num: 1}, data => data.block?.previous === "0000000000000000000000000000000000000000", ["api.openhive.network"])
+      hc.register(hiveChain?.api.block_api.get_block, {block_num: 1}, data => data.block?.previous === "0000000000000000000000000000000000000000", apiList);
+      hc.on("data", (data: Array<IScoredEndpoint>) => { console.log(JSON.stringify(data)); });
     }
-  }, [hc, hiveChain])
+  }, [hiveChain])
 
   return (
     <div>
