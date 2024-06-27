c9151ed1ef9dc0b147284a793a5a7c08ad5b762e
mcfarhat <mcfarhat@gmail.com>
2024-06-27 16:21:41 +0000

    Adjust user profile witness box list display to alphabetical sorting

--- components/account/AccountWitnessVotesCard.tsx
+++ components/account/AccountWitnessVotesCard.tsx
@@ -39,12 +39,14 @@ const buildTableBody = (voters: string[]) => {
 };
 
 const AccountWitnessVotesCard: React.FC<AccountWitnessVotesCardProps> = ({
-  voters,
+  voters: initialVoters,
 }) => {
   const [isPropertiesHidden, setIsPropertiesHidden] = useState(true);
-
+  const voters = [...initialVoters];
   if (!voters || !voters.length) return null;
-
+  voters.sort((a, b) =>
+    a.toLowerCase().localeCompare(b.toLowerCase())  // Changed: Sorting logic to ensure alphabetical order
+  );
   const handlePropertiesVisibility = () => {
     setIsPropertiesHidden(!isPropertiesHidden);
   };
