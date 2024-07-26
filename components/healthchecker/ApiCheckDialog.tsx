50309dd252672bcb185bf16e2fedfd5bb914bb4f
jlachor <jlachor@syncad.com>
2024-07-26 12:43:12 +0200

    Initializie empty api check component

---
+++ components/healthchecker/ApiCheckDialog.tsx
@@ -0,0 +1,19 @@
+
+interface ApiCheckDialogProps {
+
+}
+
+const ApiCheckDialog: React.FC<ApiCheckDialogProps> = ({
+
+}) => {
+
+  const clickEdit = () => {
+
+  }
+
+  return (
+    <></>
+  )
+};
+
+export default ApiCheckDialog;
