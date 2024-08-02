649d959a94d5c7752510fee9e602845b45eb44b5
Lukas <lukas.budginas@gmail.com>
2024-08-02 16:17:01 +0300

    update useWitnesses hook to receive sort key and order

--- api/common/useWitnesses.ts
+++ api/common/useWitnesses.ts
@@ -1,14 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import fetchingService from "@/services/FetchingService";
 
-const useWitnesses = (witnessesLimit: number) => {
+
+const useWitnesses = (
+  witnessesLimit: number,
+  orderBy: string,
+  orderIs: string
+) => {
   const {
     data: witnessesData,
     isLoading: isWitnessDataLoading,
     isError: isWitnessDataError,
   } = useQuery({
-    queryKey: ["witnesses", witnessesLimit],
-    queryFn: () => fetchingService.getWitnesses(witnessesLimit, 0, "votes", "desc"),
+    queryKey: ["witnesses", witnessesLimit, orderBy, orderIs],
+    queryFn: () =>
+      fetchingService.getWitnesses(witnessesLimit, 0, orderBy, orderIs),
     refetchOnWindowFocus: false,
   });
 
