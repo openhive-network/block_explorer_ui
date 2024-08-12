e852fe58d7f871b7c43587be3bc668a50bbc0497
Lukas <lukas.budginas@gmail.com>
2024-08-12 19:44:23 +0300

    Update hook on every route change so we can see current block sync status

--- api/homePage/useDynamicGlobal.ts
+++ api/homePage/useDynamicGlobal.ts
@@ -2,25 +2,26 @@ import { useQuery } from "@tanstack/react-query";
 import fetchingService from "@/services/FetchingService";
 import { adjustDynamicGlobalBlockData } from "@/utils/QueryDataSelectors";
 import { useHiveChainContext } from "@/components/contexts/HiveChainContext";
+import { useRouter } from "next/router";
 
 const useDynamicGlobal = (headBlockNum?: number) => {
+  const { hiveChain } = useHiveChainContext();
+  const router = useRouter();
 
-  const {hiveChain} = useHiveChainContext();
-
-    function getGlobalBlockData() {
-        return Promise.all([
-            fetchingService.getDynamicGlobalProperties(),
-            fetchingService.getCurrentPriceFeed(),
-            fetchingService.getRewardFunds(),
-        ]);
-        }
+  function getGlobalBlockData() {
+    return Promise.all([
+      fetchingService.getDynamicGlobalProperties(),
+      fetchingService.getCurrentPriceFeed(),
+      fetchingService.getRewardFunds(),
+    ]);
+  }
 
   const {
     data: dynamicGlobalData,
     isLoading: dynamicGlobalLoading,
     isError: dynamicGlobalError,
   } = useQuery({
-    queryKey: [`global`, headBlockNum],
+    queryKey: [`global`, headBlockNum, router],
     queryFn: getGlobalBlockData,
     select: (dynamicGlobalBlockData) =>
       adjustDynamicGlobalBlockData(
