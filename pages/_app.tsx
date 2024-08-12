ae626d4e8903dbe6ae1270f36948840a15a21c2a
Lukas <lukas.budginas@gmail.com>
2024-08-12 19:37:21 +0300

    Move query client provider to contexts file, remove providers file and add head block context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,14 @@
 import type { AppProps } from "next/app";
 import { ErrorBoundary } from "react-error-boundary";
 import Context from "@/components/context";
-import Providers from "@/components/providers";
 import "@/styles/globals.css";
 import ErrorPage from "@/pages/ErrorPage";
 
 export default function App({ Component, pageProps }: AppProps) {
-
   return (
     <ErrorBoundary fallback={<ErrorPage />}>
       <Context>
-        <Providers>
-          <Component {...pageProps} />
-        </Providers>
+        <Component {...pageProps} />
       </Context>
     </ErrorBoundary>
   );
