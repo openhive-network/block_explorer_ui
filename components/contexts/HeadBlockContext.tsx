2929b6c923940725ffc14701c699781f313ed1d0
Lukas <lukas.budginas@gmail.com>
2024-08-12 19:34:43 +0300

    Create head block context instead of only hook for better data live handling

---
+++ components/contexts/HeadBlockContext.tsx
@@ -0,0 +1,71 @@
+import { useQuery } from "@tanstack/react-query";
+import fetchingService from "@/services/FetchingService";
+import { createContext, useContext, useEffect } from "react";
+import { config } from "@/Config";
+import { useRouter } from "next/router";
+import { useUserSettingsContext } from "./UserSettingsContext";
+
+interface IHeadBlockContext {
+  headBlockNumberData: any;
+  headBlockNumberDataLoading: any;
+  headBlockNumberDataError: any;
+  checkTemporaryHeadBlockNumber: any;
+  refetch: any;
+}
+export const HeadBlockContext = createContext<IHeadBlockContext | undefined>(
+  undefined
+);
+
+export const useHeadBlockNumber = () => {
+  const context = useContext(HeadBlockContext);
+  if (context === undefined) {
+    throw new Error("hook must be used inside it`s context");
+  }
+
+  return context;
+};
+
+export const HeadBlockContextProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const {
+    settings: { liveData },
+  } = useUserSettingsContext();
+  const router = useRouter();
+
+  const {
+    data: headBlockNumberData,
+    isLoading: headBlockNumberDataLoading,
+    isError: headBlockNumberDataError,
+    refetch,
+  } = useQuery({
+    queryKey: ["headBlockNum", router],
+    queryFn: () => fetchingService.getHafbeLastSyncedBlock(),
+    refetchOnWindowFocus: false,
+    refetchInterval: liveData ? config.mainRefreshInterval : Infinity,
+  });
+
+  useEffect(() => {
+    if (liveData) {
+      refetch();
+    }
+  }, [liveData, refetch]);
+
+  const checkTemporaryHeadBlockNumber = async () => {
+    return await fetchingService.getHeadBlockNum();
+  };
+
+  return (
+    <HeadBlockContext.Provider
+      value={{
+        headBlockNumberData,
+        headBlockNumberDataLoading,
+        headBlockNumberDataError,
+        checkTemporaryHeadBlockNumber,
+        refetch,
+      }}
+    >
+      {children}
+    </HeadBlockContext.Provider>
+  );
+};
