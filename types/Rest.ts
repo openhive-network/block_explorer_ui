77d58a9b33cda7c5ceaec29da42c4d524a9b6cc8
jlachor <jlachor@syncad.com>
2024-08-19 14:16:55 +0200

    Repair witness property in account page

--- types/Rest.ts
+++ types/Rest.ts
@@ -15,21 +15,21 @@ export class Witness {
   witness!: string;
   rank!: number;
   url!: string;
-  vests!: number;
-  votes_hive_power!: number;
+  vests!: string;
   votes_daily_change!: number;
   votes_daily_change_hive_power!: number;
   voters_num!: number;
   voters_num_daily_change!: number;
   price_feed!: number;
   bias!: number;
-  feed_age!: string;
   feed_updated_at!: Date;
   block_size!: number;
   signing_key!: string;
   version!: string;
   missed_blocks!: number;
   hbd_interest_rate!: number;
+  vests_hive_power!: number;
+
 }
 
 export class RestGetVotersParamsReq {
