945e21a30b8e317d9704bb6511dd0a33b96f9452
jlachor <jlachor@syncad.com>
2024-08-20 12:37:50 +0200

    Use OperationTypes context as a provider component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,32 +4,6 @@ import "@/styles/globals.css";
 import Providers from "@/components/providers";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const {
-    apiAddress,
-    nodeAddress,
-    writeApiAddressToLocalStorage,
-    writeNodeAddressToLocalStorage,
-  } = useApiAddresses();
-
-  const queryClient = useMemo(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            enabled: apiAddress !== null && nodeAddress !== null,
-          },
-        },
-        queryCache: new QueryCache({
-          onError: (error) => {
-            toast.error("Error occured", {
-              description: `${(error as Error).message}`,
-            });
-          },
-        }),
-      }),
-    [apiAddress, nodeAddress]
-  );
-
   return (
     <Providers>
       <Component {...pageProps} />
