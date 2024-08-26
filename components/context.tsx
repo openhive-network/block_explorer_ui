a0ac01f64c0015174f2bb93bdbbd9c57a4930f6f
Lukas <lukas.budginas@gmail.com>
2024-08-26 18:52:46 +0300

    WIP update app and context.tsx check if query logic is required

--- components/context.tsx
+++ components/context.tsx
@@ -1,92 +1,51 @@
-import React, { ReactNode, useEffect, useState, useMemo } from "react";
+import React, { ReactNode } from "react";
+import { UserSettingsContextProvider } from "../contexts/UserSettingsContext";
+import { HiveChainContextProvider } from "../contexts/HiveChainContext";
 import {
-  UserSettings,
-  UserSettingsContext,
-} from "../contexts/UserSettingsContext";
-import { HiveChainContext } from "../contexts/HiveChainContext";
-import { IHiveChainInterface, createHiveChain } from "@hiveio/wax";
-import { AddressesContext } from "../contexts/AddressesContext";
+  AddressesContextProvider,
+} from "../contexts/AddressesContext";
+
 import useApiAddresses from "@/utils/ApiAddresses";
-import fetchingService from "@/services/FetchingService";
-import {
-  QueryClient,
-  QueryCache,
-  QueryClientProvider,
-} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { toast } from "sonner";
 import Layout from "./layout";
 import { HeadBlockContextProvider } from "@/contexts/HeadBlockContext";
 
 const Context: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [userSettings, setUserSettings] = useState<UserSettings>({
-    rawJsonView: false,
-    liveData: false,
-    prettyJsonView: false,
-  });
-  const [hiveChain, setHiveChain] = useState<IHiveChainInterface | undefined>(
-    undefined
-  );
 
-  const createChain = async () => {
-    const chain = await createHiveChain();
-    setHiveChain(chain);
-    fetchingService.setHiveChain(chain);
-  };
-
-  useEffect(() => {
-    createChain();
-  }, []);
-
-  const {
-    apiAddress,
-    nodeAddress,
-    writeApiAddressToLocalStorage,
-    writeNodeAddressToLocalStorage,
-  } = useApiAddresses();
-
-  const queryClient = useMemo(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            enabled: apiAddress !== null && nodeAddress !== null,
-          },
-        },
-        queryCache: new QueryCache({
-          onError: (error) => {
-            toast.error("Error occured", {
-              description: `${(error as Error).message}`,
-            });
-          },
-        }),
-      }),
-    [apiAddress, nodeAddress]
-  );
+  // const queryClient = useMemo(
+  //   () =>
+  //     new QueryClient({
+  //       defaultOptions: {
+  //         queries: {
+  //           enabled: apiAddress !== null && nodeAddress !== null,
+  //         },
+  //       },
+  //       queryCache: new QueryCache({
+  //         onError: (error) => {
+  //           toast.error("Error occured", {
+  //             description: `${(error as Error).message}`,
+  //           });
+  //         },
+  //       }),
+  //     }),
+  //   [apiAddress, nodeAddress]
+  // );
 
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <UserSettingsContext.Provider
-          value={{ settings: userSettings, setSettings: setUserSettings }}
-        >
-          <HeadBlockContextProvider>
-            <AddressesContext.Provider
-              value={{
-                apiAddress: apiAddress,
-                setApiAddress: writeApiAddressToLocalStorage,
-                nodeAddress: nodeAddress,
-                setNodeAddress: writeNodeAddressToLocalStorage,
-              }}
-            >
-              <HiveChainContext.Provider value={{ hiveChain, setHiveChain }}>
-                <Layout>{children}</Layout>
-                <ReactQueryDevtools initialIsOpen={false} />
-              </HiveChainContext.Provider>
-            </AddressesContext.Provider>
-          </HeadBlockContextProvider>
-        </UserSettingsContext.Provider>
-      </QueryClientProvider>
+      {/* <QueryClientProvider client={queryClient}> */}
+      <UserSettingsContextProvider>
+        <HeadBlockContextProvider>
+          <AddressesContextProvider>
+            <HiveChainContextProvider>
+              <Layout>{children}</Layout>
+              <ReactQueryDevtools initialIsOpen={false} />
+            </HiveChainContextProvider>
+          </AddressesContextProvider>
+        </HeadBlockContextProvider>
+      </UserSettingsContextProvider>
+      {/* </QueryClientProvider> */}
     </>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,44 @@ import { ErrorBoundary } from "react-error-boundary";
 import Context from "@/components/context";
 import "@/styles/globals.css";
 import ErrorPage from "@/pages/ErrorPage";
+import {
+  // QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import useApiAddresses from "@/utils/ApiAddresses";
+// import { useMemo } from "react";
+// import { toast } from "sonner";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  // const { apiAddress, nodeAddress } = useApiAddresses();
+  // const queryClient = useMemo(
+  //   () =>
+  //     new QueryClient({
+  //       defaultOptions: {
+  //         queries: {
+  //           enabled: apiAddress !== null && nodeAddress !== null,
+  //         },
+  //       },
+  //       queryCache: new QueryCache({
+  //         onError: (error: any) => {
+  //           toast.error("Error occured", {
+  //             description: `${(error as Error).message}`,
+  //           });
+  //         },
+  //       }),
+  //     }),
+  //   [apiAddress, nodeAddress]
+  // );
   return (
     <ErrorBoundary fallback={<ErrorPage />}>
-      <Context>
-        <Component {...pageProps} />
-      </Context>
+      <QueryClientProvider client={queryClient}>
+        <Context>
+          <Component {...pageProps} />
+        </Context>
+      </QueryClientProvider>
     </ErrorBoundary>
   );
 }
