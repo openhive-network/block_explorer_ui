13e9cf23d86b79419f2d0b342c1757ca6e5d26ca
Lukas <lukas.budginas@gmail.com>
2024-08-26 18:51:34 +0300

    Refactor contexts

---
+++ contexts/AddressesContext.tsx
@@ -0,0 +1,50 @@
+import useApiAddresses from "@/utils/ApiAddresses";
+import { createContext, useContext } from "react";
+
+export type AddressesContextType = {
+  apiAddress: string | null;
+  setApiAddress: (address: string | null) => void;
+  nodeAddress: string | null;
+  setNodeAddress: (address: string | null) => void;
+};
+
+export const AddressesContext = createContext<AddressesContextType>({
+  apiAddress: "",
+  setApiAddress: () => {},
+  nodeAddress: "",
+  setNodeAddress: () => {},
+});
+
+export const useAddressesContext = () => {
+  const context = useContext(AddressesContext);
+
+  if (context === undefined) {
+    throw new Error("hook must be used inside it`s context");
+  }
+
+  return context;
+};
+
+export const AddressesContextProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const {
+    apiAddress,
+    nodeAddress,
+    writeApiAddressToLocalStorage,
+    writeNodeAddressToLocalStorage,
+  } = useApiAddresses();
+
+  return (
+    <AddressesContext.Provider
+      value={{
+        apiAddress: apiAddress,
+        setApiAddress: writeApiAddressToLocalStorage,
+        nodeAddress: nodeAddress,
+        setNodeAddress: writeNodeAddressToLocalStorage,
+      }}
+    >
+      {children}
+    </AddressesContext.Provider>
+  );
+};
