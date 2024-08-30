c0173873ccbab8184f70bbc17762500d5f0fcd42
Lukas <lukas.budginas@gmail.com>
2024-08-30 14:18:58 +0300

    Create account name card for mobile view

---
+++ components/account/MobileAccountNameCard.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+import useAccountDetails from "@/api/accountPage/useAccountDetails";
+import { Card, CardHeader } from "../ui/card";
+import { getHiveAvatarUrl } from "@/utils/HiveBlogUtils";
+
+interface MobileAccountNameCardProps {
+  accountName: string;
+}
+
+const MobileAccountNameCard: React.FC<MobileAccountNameCardProps> = ({
+  accountName,
+}) => {
+  const { accountDetails } = useAccountDetails(accountName);
+  if (!accountDetails) return;
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex justify-between bg-explorer-dark-gray text-explorer-orange text-2xl my-4">
+          {accountDetails.is_witness ? (
+            <div data-testid="account-name">
+              {accountDetails.name} <span className="text-sm">(witness)</span>
+            </div>
+          ) : (
+            <div>{accountDetails.name}</div>
+          )}
+          <span>
+            <Image
+              className="rounded-full border-2 border-explorer-orange"
+              src={getHiveAvatarUrl(accountName)}
+              alt="avatar"
+              width={50}
+              height={50}
+              data-testid="user-avatar"
+            />
+          </span>
+        </div>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default MobileAccountNameCard;
