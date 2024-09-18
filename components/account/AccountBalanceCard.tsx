26d99e6c50ab89de9aa9c5c683d4aef2bc68ba6a
Lukas <lukas.budginas@gmail.com>
2024-09-18 16:07:36 +0300

    Add hide/show feature for account wallet section

--- components/account/AccountBalanceCard.tsx
+++ components/account/AccountBalanceCard.tsx
@@ -1,8 +1,8 @@
-import { ReactNode, Fragment } from "react";
+import { Fragment, useState } from "react";
+import { ArrowDown, ArrowUp } from "lucide-react";
 
 import { Card, CardContent, CardHeader } from "../ui/card";
 import { Table, TableBody, TableCell, TableRow } from "../ui/table";
-import { VEST_HP_KEYS_MAP } from "@/hooks/common/useConvertedAccountDetails";
 import VestsTooltip from "../VestsTooltip";
 import Explorer from "@/types/Explorer";
 import { changeHBDToDollarsDisplay } from "@/utils/StringUtils";
@@ -26,42 +26,58 @@ const cardNameMap = new Map([
   ["vesting_withdraw_rate", "Powering down HP"],
 ]);
 
-
-
 const AccountBalanceCard: React.FC<AccountBalanceCardProps> = ({
   header,
   userDetails,
 }) => {
-
-  const keys = Object.keys(userDetails) as (keyof Explorer.AccountDetailsDollars)[];
+  const keys = Object.keys(
+    userDetails
+  ) as (keyof Explorer.AccountDetailsDollars)[];
 
   const renderKey = (key: keyof Explorer.FormattedAccountDetails) => {
     if (Object.keys(userDetails.vests).includes(key)) {
-      const vestKey = key as keyof Explorer.AccountDetailsVests
-      const vestValue = userDetails.vests[vestKey]
-      return <VestsTooltip tooltipTrigger={userDetails[key] as string} tooltipContent={vestValue } />
+      const vestKey = key as keyof Explorer.AccountDetailsVests;
+      const vestValue = userDetails.vests[vestKey];
+
+      return (
+        <VestsTooltip
+          tooltipTrigger={userDetails[key] as string}
+          tooltipContent={vestValue}
+        />
+      );
     }
-    return <>{userDetails[key]}</>;
+    return <>{userDetails[key]} </>;
   };
+
+  const [isBalancesHidden, setIsBalancesHidden] = useState(false);
 
   const buildTableBody = (
-    parameters: (keyof Explorer.AccountDetailsDollars)[],
+    parameters: (keyof Explorer.AccountDetailsDollars)[]
   ) => {
-    return parameters.map((param: keyof Explorer.AccountDetailsDollars, index: number) => {
-      if (cardNameMap.has(param)) {
-        return (
-          <Fragment key={index}>
-            <TableRow className="border-b border-gray-700 hover:bg-inherit">
-              <TableCell>{cardNameMap.get(param)}</TableCell>
-              <TableCell className="text-right">{renderKey(param as keyof Explorer.FormattedAccountDetails)}</TableCell>
-              <TableCell className="text-right">{changeHBDToDollarsDisplay(userDetails.dollars[param])}</TableCell>
-            </TableRow>
-          </Fragment>
-        );
+    return parameters.map(
+      (param: keyof Explorer.AccountDetailsDollars, index: number) => {
+        if (cardNameMap.has(param)) {
+          return (
+            <Fragment key={index}>
+              <TableRow className="border-b border-gray-700 hover:bg-inherit">
+                <TableCell>{cardNameMap.get(param)}</TableCell>
+                <TableCell className="text-right">
+                  {renderKey(param as keyof Explorer.FormattedAccountDetails)}
+                </TableCell>
+                <TableCell className="text-right">
+                  {changeHBDToDollarsDisplay(userDetails.dollars[param])}
+                </TableCell>
+              </TableRow>
+            </Fragment>
+          );
+        }
       }
-    });
+    );
   };
 
+  const handleBalancesVisibility = () => {
+    setIsBalancesHidden(!isBalancesHidden);
+  };
 
   return (
     <Card
@@ -69,11 +85,18 @@ const AccountBalanceCard: React.FC<AccountBalanceCardProps> = ({
       className="overflow-hidden pb-0"
     >
       <CardHeader className="p-0">
-        <div className="flex justify-between align-center p-2 hover:bg-slate-600 cursor-pointer px-4">
+        <div
+          onClick={handleBalancesVisibility}
+          className="flex justify-between align-center p-2 hover:bg-slate-600 cursor-pointer px-4"
+        >
           <div className="text-lg">{header}</div>
+          {isBalancesHidden ? <ArrowDown /> : <ArrowUp />}
         </div>
       </CardHeader>
-      <CardContent data-testid="card-content">
+      <CardContent
+        hidden={isBalancesHidden}
+        data-testid="card-content"
+      >
         <Table>
           <TableBody>{buildTableBody(keys)}</TableBody>
         </Table>
