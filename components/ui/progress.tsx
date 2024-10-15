2b2b0f37f1f5a2204c892dd9b26076d21c5e960b
Lukas <lukas.budginas@gmail.com>
2024-10-15 15:36:05 +0300

    Add progress bar z index

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -11,7 +11,7 @@ const Progress = React.forwardRef<
     <ProgressPrimitive.Root
       ref={ref}
       className={cn(
-        `relative h-4 w-full overflow-hidden rounded-full bg-explorer-light-gray dark:bg-[#03182c]`,
+        `relative h-4 w-full overflow-hidden rounded-full bg-explorer-light-gray dark:bg-[#03182c] z-0`,
         className
       )}
       {...props}
