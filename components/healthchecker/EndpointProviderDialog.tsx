d4b303fde584ce86720b67e04f154389ca7a5589
jlachor <jlachor@syncad.com>
2024-11-04 12:28:00 +0100

    Create new component for endpoint provider change

---
+++ components/healthchecker/EndpointProviderDialog.tsx
@@ -0,0 +1,38 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+  } from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
+import { ApiChecker } from "./HealthChecker";
+import { Toggle } from "../ui/toggle";
+import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+
+interface EndpointProviderDialogProps {
+  className?: string;
+  isOpened: boolean;
+  onDialogOpenChange: (isOpened: boolean, provider?: string) => void;
+}
+
+const EndpointProviderDialog: React.FC<EndpointProviderDialogProps> = ({
+  className,
+  isOpened,
+  onDialogOpenChange
+}) => {
+
+
+  return (
+    <Dialog open={isOpened} onOpenChange={onDialogOpenChange}>
+      <DialogContent className={cn(className, "bg-explorer-bg-start")}>
+        <DialogHeader><DialogTitle>Endpoint link</DialogTitle></DialogHeader>
+        <div>
+
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+};
+
+export default EndpointProviderDialog;
