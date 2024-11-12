8a5195b8d9c75afcc89540c01d9ee988eb5f85bd
Lukas <lukas.budginas@gmail.com>
2024-11-12 18:41:44 +0200

    Update searches section

--- components/home/SearchesSection.tsx
+++ components/home/SearchesSection.tsx
@@ -1,3 +1,12 @@
+// TODO:
+//  This component should be devided to separate components
+// Create seprate components to handle logic by search
+// Create separate components for search results based on search
+
+// TODO#2:
+// Logic for url bulding could be simplified by using URLSearchParams API
+// logic in some useEffects also should be simplified
+
 import { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 
@@ -22,6 +31,7 @@ import JumpToPage from "../JumpToPage";
 import BlockSearch from "./searches/BlockSearch";
 import AccountSearch from "./searches/AccountSearch";
 import CommentsSearch from "./searches/CommentsSearch";
+import CommentsPermlinkSearch from "./searches/CommentPermlinkSearch";
 import OperationsTable from "../OperationsTable";
 import CustomPagination from "../CustomPagination";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
@@ -32,6 +42,8 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "../ui/accordion";
+import usePermlinkSearch from "@/hooks/api/common/usePermlinkSearch";
+import CommentPermlinkResultTable from "./searches/CommentPermlinkResultTable";
 
 interface SearchesSectionProps {}
 
@@ -49,13 +61,16 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
     number | undefined
   >(undefined);
   const [lastSearchKey, setLastSearchKey] = useState<
-    "block" | "account" | "comment" | undefined
+    "block" | "account" | "comment" | "comment-permlink" | undefined
   >(undefined);
   const [blockSearchProps, setBlockSearchProps] = useState<
     Explorer.BlockSearchProps | undefined
   >(undefined);
   const [commentSearchProps, setCommentSearchProps] = useState<
     Explorer.CommentSearchProps | undefined
+  >(undefined);
+  const [permlinkSearchProps, setPermlinkSearchProps] = useState<
+    Explorer.PermlinkSearchProps | undefined
   >(undefined);
   const [accountOperationsSearchProps, setAccountOperationsSearchProps] =
     useState<Explorer.AccountSearchOperationsProps | undefined>(undefined);
@@ -64,7 +79,13 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
   const searchesRef = useRef<HTMLDivElement | null>(null);
 
   const { operationsTypes } = useOperationTypes() || [];
-  const commentSearch = useCommentSearch(commentSearchProps);
+
+  const { permlinkSearchData, permlinkSearchDataLoading } =
+    usePermlinkSearch(permlinkSearchProps);
+
+  const { commentSearchData, commentSearchDataLoading } =
+    useCommentSearch(commentSearchProps);
+
   const { blockSearchData, blockSearchDataLoading } =
     useBlockSearch(blockSearchProps);
 
@@ -76,7 +97,7 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
     accountOperations.accountOperations
   ) as Hive.AccountOperationsResponse;
   const formattedCommentOperations = useOperationsFormatter(
-    commentSearch.commentSearchData
+    commentSearchData
   ) as Hive.CommentOperationResponse;
 
   useEffect(() => {
@@ -93,9 +114,12 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
         !accountOperations.isAccountOperationsLoading &&
         lastSearchKey === "account") ||
       (!blockSearchDataLoading && lastSearchKey === "block") ||
-      (commentSearch &&
-        !commentSearch.commentSearchDataLoading &&
+      (commentSearchData &&
+        !commentSearchDataLoading &&
         lastSearchKey === "comment");
+    permlinkSearchData &&
+      !permlinkSearchDataLoading &&
+      lastSearchKey === "comment-permlink";
     if (isAllSearchLoading && wasActualized) {
       searchesRef.current?.scrollIntoView({
         behavior: "smooth",
@@ -105,9 +129,12 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
     }
   }, [
     accountOperations,
+    permlinkSearchData,
+    permlinkSearchDataLoading,
     lastSearchKey,
     blockSearchDataLoading,
-    commentSearch,
+    commentSearchData,
+    commentSearchDataLoading,
     isAllSearchLoading,
   ]);
 
@@ -128,6 +155,20 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
     setCommentPaginationPage(1);
     setPreviousCommentSearchProps(props);
     setLastSearchKey("comment");
+  };
+
+  const startCommentPermlinkSearch = async (
+    commentPermlinkSearchProps: Explorer.CommentPermlinSearchParams
+  ) => {
+    const { ...params } = commentPermlinkSearchProps;
+    const props: Explorer.PermlinkSearchProps = {
+      ...params,
+      accountName: params.accountName || "",
+    };
+    setIsAllSearchLoading(true);
+    setPermlinkSearchProps(props);
+    setCommentPaginationPage(1);
+    setLastSearchKey("comment-permlink");
   };
 
   const startAccountOperationsSearch = async (
@@ -347,6 +388,20 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
                 />
               </AccordionContent>
             </AccordionItem>
+            <AccordionItem value="comment-permlink">
+              <AccordionTrigger className="p-3 mb-2">
+                Comment permlink search
+              </AccordionTrigger>
+              <AccordionContent className="px-2 flex flex-col gap-y-4">
+                <CommentsPermlinkSearch
+                  startCommentPermlinkSearch={(params) =>
+                    startCommentPermlinkSearch(params)
+                  }
+                  loading={permlinkSearchDataLoading}
+                  searchRanges={searchRanges}
+                />
+              </AccordionContent>
+            </AccordionItem>
             <AccordionItem value="comment">
               <AccordionTrigger className="p-3 mb-2">
                 Comment search
@@ -355,7 +410,7 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
                 <CommentsSearch
                   startCommentsSearch={(params) => startCommentSearch(params)}
                   operationsTypes={operationsTypes}
-                  loading={commentSearch.commentSearchDataLoading}
+                  loading={commentSearchDataLoading}
                   searchRanges={searchRanges}
                 />
               </AccordionContent>
@@ -399,9 +454,30 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
             </div>
           </div>
         )}
-        {!!commentSearch.commentSearchData &&
+        {permlinkSearchData && lastSearchKey === "comment-permlink" && (
+          <div>
+            <div
+              className="text-center my-5"
+              data-testid="result-section-header"
+            >
+              Results:
+            </div>
+            <div className="flex flex-wrap">
+              {permlinkSearchData.total_permlinks > 0 ? (
+                <CommentPermlinkResultTable
+                  data={permlinkSearchData.permlinks_result}
+                />
+              ) : (
+                <div className="flex justify-center w-full">
+                  No permlinks matching given criteria
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+        {commentSearchData &&
           lastSearchKey === "comment" &&
-          (!!commentSearch.commentSearchData.total_operations ? (
+          (!!commentSearchData?.total_operations ? (
             <div>
               <Link href={getCommentPageLink()}>
                 <Button data-testid="go-to-result-page">
@@ -412,7 +488,7 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
               <div className="flex justify-center items-center text-black dark:text-white">
                 <CustomPagination
                   currentPage={commentPaginationPage}
-                  totalCount={commentSearch.commentSearchData?.total_operations}
+                  totalCount={commentSearchData.total_operations}
                   pageSize={config.standardPaginationSize}
                   onPageChange={changeCommentSearchPagination}
                 />
@@ -429,7 +505,7 @@ const SearchesSection: React.FC<SearchesSectionProps> = ({}) => {
                   formattedCommentOperations?.operations_result
                 )}
                 unformattedOperations={convertCommentsOperationResultToTableOperations(
-                  commentSearch.commentSearchData.operations_result
+                  commentSearchData.operations_result
                 )}
               />
             </div>
