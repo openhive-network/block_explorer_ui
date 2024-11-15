2702747d1a633704f4d54617b74ba60606192656
Lukas <lukas.budginas@gmail.com>
2024-11-15 16:21:15 +0200

    Update last updated wintess tooltip trigger

--- components/LastUpdatedWitnessIcon.tsx
+++ components/LastUpdatedWitnessIcon.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 import {
   Tooltip,
   TooltipProvider,
   TooltipTrigger,
   TooltipContent,
 } from "@/components/ui/tooltip";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 import moment from "moment";
 
 type LastUpdatedProps = {
@@ -15,30 +15,32 @@ type LastUpdatedProps = {
 
 const calculateTimeDiff = (date: string | Date) => {
   if (!date) return 0;
-  const timeDiff = moment.utc(date).diff(moment.utc(), 'minutes'); // Calculate the difference in minutes
+  const timeDiff = moment.utc(date).diff(moment.utc(), "minutes"); // Calculate the difference in minutes
   return Math.abs(timeDiff);
 };
 
-const LastUpdatedWitnessIcon: React.FC<LastUpdatedProps> = ({ lastUpdatedAt }) => {
+const LastUpdatedWitnessIcon: React.FC<LastUpdatedProps> = ({
+  lastUpdatedAt,
+}) => {
   const timeDiff = calculateTimeDiff(lastUpdatedAt);
   const iconRef = useRef<SVGSVGElement | null>(null);
 
   const getIconColor = (timeDiff: number) => {
-    let colorClass = '';
-    let fillColor = '';
+    let colorClass = "";
+    let fillColor = "";
 
     switch (true) {
       case timeDiff <= 10:
-        colorClass = 'text-green-500 ';
-        fillColor = '#48bb78'; // Green
+        colorClass = "text-green-500 ";
+        fillColor = "#48bb78"; // Green
         break;
       case timeDiff <= 60:
-        colorClass = 'text-orange-500 bold';
-        fillColor = '#ed8936'; // Orange
+        colorClass = "text-orange-500 bold";
+        fillColor = "#ed8936"; // Orange
         break;
       default:
-        colorClass = 'text-red-500 bold';
-        fillColor = '#f56565'; // Red
+        colorClass = "text-red-500 bold";
+        fillColor = "#f56565"; // Red
         break;
     }
 
@@ -47,15 +49,17 @@ const LastUpdatedWitnessIcon: React.FC<LastUpdatedProps> = ({ lastUpdatedAt }) =
 
   useEffect(() => {
     if (iconRef.current) {
-      const path = iconRef.current.querySelector('path');
+      const path = iconRef.current.querySelector("path");
       if (path) {
-        path.setAttribute('fill', getIconColor(timeDiff).fillColor);
+        path.setAttribute("fill", getIconColor(timeDiff).fillColor);
       }
     }
   }, [timeDiff]);
 
   const { colorClass } = getIconColor(timeDiff);
-  const tooltipMessage = `Last Updated - ${timeDiff} ${timeDiff === 1 ? 'min' : 'mins'} ago`;
+  const tooltipMessage = `Last Updated - ${timeDiff} ${
+    timeDiff === 1 ? "min" : "mins"
+  } ago`;
 
   return (
     <div className="flex items-center space-x-2">
@@ -70,15 +74,23 @@ const LastUpdatedWitnessIcon: React.FC<LastUpdatedProps> = ({ lastUpdatedAt }) =
                 ref={iconRef}
                 className={colorClass}
               />
+              <span className={`${colorClass} font-bold ml-2`}>{`${timeDiff} ${
+                timeDiff === 1 ? "min" : "mins"
+              } ago`}</span>
             </span>
           </TooltipTrigger>
-          <TooltipContent side="top" align="start" sideOffset={5} alignOffset={10}>
+          <TooltipContent
+            side="top"
+            align="start"
+            sideOffset={5}
+            alignOffset={10}
+            className="border-0"
+          >
             <div className="bg-theme text-text p-2 ml-3">
               <p>{tooltipMessage}</p>
             </div>
           </TooltipContent>
         </Tooltip>
-        <span className={`${colorClass} font-bold`}>{`${timeDiff} ${timeDiff === 1 ? 'min' : 'mins'} ago`}</span>
       </TooltipProvider>
     </div>
   );
