eccd9346e7d1a30bbbc284060cb53b34a4836c77
Lukas <lukas.budginas@gmail.com>
2024-11-28 19:27:47 +0200

    Create custom hook to fetch post content data

---
+++ hooks/api/postPage/usePostContent.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import fetchingService from "@/services/FetchingService";
+
+const usePostContent = (accountName: string, permlink: string) => {
+  const trimAccount = accountName.replace("@", "");
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["blockSearch", trimAccount, permlink],
+    queryFn: () => fetchPostContent(trimAccount, permlink),
+    refetchOnWindowFocus: false,
+  });
+
+  const fetchPostContent = async (accountName: string, permlink: string) => {
+    if (!accountName || !permlink) return null;
+
+    const response = await fetchingService.getContent(accountName, permlink);
+    return response;
+  };
+
+  return { data, isLoading, isError };
+};
+
+export default usePostContent;
