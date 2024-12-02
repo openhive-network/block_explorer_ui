a8f159b8185e3c0735c26db57a4fc5b433e3fe9b
jlachor <jlachor@syncad.com>
2024-12-02 13:33:39 +0100

    Restore full list of providers

--- pages/healthchecker.tsx
+++ pages/healthchecker.tsx
@@ -18,6 +18,16 @@ export default function HealthcheckerPage() {
       "https://api.hive.blog",
       "https://api.openhive.network",
       "https://anyx.io",
+      "https://rpc.ausbit.dev",
+      "https://rpc.mahdiyari.info",
+      "https://techcoderx.com",
+      "https://hive.roelandp.nl",
+      "https://hived.emre.sh",
+      "https://api.deathwing.me",
+      "https://api.c0ff33a.uk",
+      "https://hive-api.arcange.eu",
+      "https://hive-api.3speak.tv",
+      "https://hiveapi.actifit.io"
     ];
 
   const checksMap = new Map<string, ApiChecker>()
