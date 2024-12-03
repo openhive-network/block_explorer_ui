3cb8c360b19866ac4a9ee117a0d572a66cfec3dd
Bartek Wrona <wrona@syncad.com>
2024-12-03 01:21:25 +0100

    JSONRPC Apis should be defined using `TWaxApiRequest` NOT `TWaxRESTApiRequest`

--- types/Node.ts
+++ types/Node.ts
@@ -1,23 +1,23 @@
 import {
-    TWaxRestApiRequest,
+  TWaxApiRequest
   } from "@hiveio/wax";
   import Hive from "@/types/Hive";
 
 export type ExplorerNodeApi = {
     database_api: {
-      get_reward_funds: TWaxRestApiRequest<{}, { funds: Hive.RewardFunds[] }>;
-      get_current_price_feed: TWaxRestApiRequest<{}, Hive.PriceFeed>;
-      find_vesting_delegations: TWaxRestApiRequest<
-        { account: string },
-        { delegations: Hive.VestingDelegations[] }
-      >;
-      get_witness_schedule: TWaxRestApiRequest<
-        { id: number },
-        Hive.WitnessesSchedule
-      >;
-    };
+      get_reward_funds: TWaxApiRequest<{}, { funds: Hive.RewardFunds[] }>;
+      get_current_price_feed: TWaxApiRequest<{}, Hive.PriceFeed>;
+      find_vesting_delegations: TWaxApiRequest<
+          { account: string },
+          { delegations: Hive.VestingDelegations[] }
+        >;
+      get_witness_schedule: TWaxApiRequest<
+          { id: number },
+          Hive.WitnessesSchedule
+        >;
+      };
     rc_api: {
-      list_rc_direct_delegations: TWaxRestApiRequest<
+      list_rc_direct_delegations: TWaxApiRequest<
         { start: [string, string]; limit: number },
         { rc_direct_delegations: Hive.RCDelegations[] }
       >;
