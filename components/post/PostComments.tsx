0e5446208a86470e4b28c40d7da5ce9659a06f6f
Lukas <lukas.budginas@gmail.com>
2025-01-06 18:02:23 +0200

    Update PostComments component to show nested comments

--- components/post/PostComments.tsx
+++ components/post/PostComments.tsx
@@ -1,28 +1,73 @@
+import React from "react";
+import { useRouter } from "next/router";
+
 import Hive from "@/types/Hive";
 import PostContent from "./PostContent";
+import usePostContentReplies from "@/hooks/api/postPage/usePostContentReplies";
 
-interface PostCommentsProps {
-  comments: Hive.Content[];
+interface NestedCommentProps {
+  comment: Hive.Content;
 }
 
-const PostComments: React.FC<PostCommentsProps> = ({ comments }) => {
-  if (!comments || !comments.length) return null;
+const NestedComment: React.FC<NestedCommentProps> = ({ comment }) => {
+  const shouldFetch = comment.children > 0;
 
-  return comments.map((comment) => {
-    return (
-      <div
-        className="flex mt-4 justify-center"
-        key={comment.id}
-      >
-        <div className="w-[70%]">
-          <PostContent
-            active_votes={comment.active_votes}
-            data={comment}
-          />
-        </div>
+  const { data: nestedReplies } = usePostContentReplies(
+    comment.author,
+    comment.permlink,
+
+    {
+      enabled: shouldFetch,
+    }
+  );
+
+  return (
+    <div
+      className="flex mt-4 justify-end"
+      key={comment.id}
+    >
+      <div className="w-[90%]">
+        <PostContent
+          isComment={true}
+          active_votes={comment.active_votes}
+          data={comment}
+        />
+
+        {shouldFetch && nestedReplies && nestedReplies.length > 0
+          ? nestedReplies.map((child) => (
+              <NestedComment
+                key={child.id}
+                comment={child}
+              />
+            ))
+          : null}
       </div>
-    );
-  });
+    </div>
+  );
+};
+
+const PostComments = () => {
+  const router = useRouter();
+  const { post } = router?.query;
+  let accountName = post?.[1] ?? "";
+  let permlink = post?.[2] ?? "";
+
+  const { data: comments } = usePostContentReplies(accountName, permlink);
+
+  if (!comments || !comments.length) {
+    return null;
+  }
+
+  return (
+    <>
+      {comments.map((comment) => (
+        <NestedComment
+          key={comment.id}
+          comment={comment}
+        />
+      ))}
+    </>
+  );
 };
 
 export default PostComments;
