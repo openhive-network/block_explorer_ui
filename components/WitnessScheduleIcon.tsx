64b817d193c373cc35a6943c2b13a2724e354e76
Dima Rifai <dima.rifai@gmail.com>
2025-02-07 15:15:51 +0200

    Issue #451 - Replace fontawesome icon

--- components/WitnessScheduleIcon.tsx
+++ components/WitnessScheduleIcon.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import Link from "next/link";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import {
   Tooltip,
   TooltipProvider,
   TooltipTrigger,
   TooltipContent,
 } from "@/components/ui/tooltip";
+import { CalendarDays } from "lucide-react";
 
 const WitnessScheduleIcon: React.FC = () => {
   const [showTooltip, setShowTooltip] = useState<boolean>(false);
@@ -28,17 +27,12 @@ const WitnessScheduleIcon: React.FC = () => {
                   className="text-explorer-blue"
                   href="/schedule"
                   data-testid="witness-schedule-link">
-                  <FontAwesomeIcon
-                    icon={faCalendarAlt}
-                    size="lg" 
-                    onClick={handleTooltipToggle} // Toggle tooltip on click
-                    onTouchStart={handleTooltipToggle} // Toggle tooltip on touch for mobile
-                   />
+                  <CalendarDays size={20} strokeWidth={3} onClick={handleTooltipToggle} onTouchStart={handleTooltipToggle} />
                 </Link>
               </span>
             </TooltipTrigger>
             <TooltipContent side="top" align="start" sideOffset={5} alignOffset={10}>
-              <div className="bg-theme text-text p-2 ml-3">
+              <div className="bg-theme text-text p-1 ml-1">
                 <p>
                   Check{" "}
                   <Link
