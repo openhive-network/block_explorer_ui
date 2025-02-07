ede6ce27da8db210ebe4940ed16edb240a136805
Dima Rifai <dima.rifai@gmail.com>
2025-02-07 15:24:07 +0200

    Issue #451 - Replace fontawesome icon

--- components/home/CurrentBlockCard.tsx
+++ components/home/CurrentBlockCard.tsx
@@ -5,12 +5,8 @@ import Hive from "@/types/Hive";
 import Image from "next/image";
 import Link from "next/link";
 import { getHiveAvatarUrl } from "@/utils/HiveBlogUtils";
-import {
-  faCube,
-  faBoxes,
-  faExchangeAlt,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { ArrowRightLeft, Box, Boxes } from "lucide-react";
 
 interface CurrentBlockCardProps {
   blockDetails?: Hive.BlockDetails;
@@ -36,7 +32,7 @@ const CurrentBlockCard: React.FC<CurrentBlockCardProps> = ({
       <div className="flex justify-between items-center mt-1 min-h-[35px] flex-wrap">
         {/* Block Number and Icon */}
         <div className="flex items-center space-x-1">
-          <FontAwesomeIcon icon={faCube} size="sm" />
+          <Box size={15}/>
           <Link
             href={`/block/${blockDetails?.block_num}`}
             data-testid="block-number-link"
@@ -82,18 +78,18 @@ const CurrentBlockCard: React.FC<CurrentBlockCardProps> = ({
       {/* Operations and Transactions Info  */}
       <div className="flex flex-col justify-end space-y-2 pt-4 min-h-[40px]">
         <div className="flex items-center justify-end">
-          <div className="min-w-[120px]">
-            <FontAwesomeIcon icon={faBoxes} size="xs" />
-            <span className="ml-1">Operations: </span>
+          <div className="min-w-[120px] flex items-center">
+            <Boxes size={14} />
+            <span className="mx-1">Operations:</span>
             <span className="font-semibold text-sm">
               {opcount ? opcount : ""}
             </span>
           </div>
         </div>
         <div className="flex items-center justify-end">
-          <div className="min-w-[120px]">
-            <FontAwesomeIcon icon={faExchangeAlt} size="xs" />
-            <span className="ml-1">Trxs: </span>
+          <div className="min-w-[120px] flex items-center">
+            <ArrowRightLeft size={14} />
+            <span className="mx-1">Trxs:</span>
             <span className="font-semibold text-sm">
               {transactionCount}
             </span>
