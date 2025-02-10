7fa22e265f1c1f379cb6425bd24361066ff919ff
jlachor <jlachor@syncad.com>
2025-02-10 14:15:42 +0100

    Set new validators

--- contexts/HealthCheckerContext.tsx
+++ contexts/HealthCheckerContext.tsx
@@ -87,37 +87,38 @@ const apiCheckers: ApiChecker[] = [
     title: "Reward Funds",
     method: extendedHiveChain?.api.database_api.get_reward_funds,
     params: {}, 
-    validatorFunction: data => !!data ? true : data,
+    validatorFunction: data => !!data.funds ? true : "Reward funds error",
   },
   {
     title: "Dynamic Global",
     method: extendedHiveChain?.api.database_api.get_dynamic_global_properties,
     params: {}, 
-    validatorFunction: data => data.last_irreversible_block_num === 93206822 ? true : "Dynamic global error",
+    validatorFunction: data => data.id === 0 ? true : "Dynamic global error",
   },
   {
     title: "Price Feed",
     method: extendedHiveChain?.api.database_api.get_current_price_feed,
     params: {}, 
-    validatorFunction: data => !!data ? true : data,
+    validatorFunction: data => !!data.base ? true : "Price feed error",
   },
   {
     title: "Witness Schedule",
     method: extendedHiveChain?.api.database_api.get_witness_schedule,
     params: { id: 1 }, 
     validatorFunction: data => data.max_voted_witnesses === 21 ? true : "Witness schedule error",
+    // This is left wrong on purpose for tests
   },
   {
     title: "Vesting Delegations",
     method: extendedHiveChain?.api.database_api.find_vesting_delegations,
     params: { account: "hiveio" }, 
-    validatorFunction: data => !!data ? true : data,
+    validatorFunction: data => !!data.delegations ? true : "Vesting delegations error",
   },
   {
     title: "RC Direct Delegations",
     method: extendedHiveChain?.api.rc_api.list_rc_direct_delegations,
     params: { start: ["hiveio", ""], limit: 1000 }, 
-    validatorFunction: data => !!data ? true : data,
+    validatorFunction: data => !!data.rc_direct_delegations ? true : "RC delegation error",
   }
 ]
 
