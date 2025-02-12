5ed55795430e60580a34ca03963234451e6f9588
Lukas <lukas.budginas@gmail.com>
2025-02-12 17:06:24 +0200

    Create tooltip for hive values converted from vests

---
+++ components/HiveTooltip.tsx
@@ -0,0 +1,35 @@
+// Vests to Hive tooltip
+
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipTrigger,
+  TooltipContent,
+} from "./ui/tooltip";
+
+interface VestsTooltipProps {
+  tooltipTrigger: string;
+  tooltipContent: string;
+}
+
+const HiveTooltip: React.FC<VestsTooltipProps> = ({
+  tooltipTrigger,
+  tooltipContent,
+}) => {
+  return (
+    <>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <span className="inline">{tooltipTrigger}</span>
+          </TooltipTrigger>
+          <TooltipContent className="bg-theme text-text border-0">
+            <p>{tooltipContent}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </>
+  );
+};
+
+export default HiveTooltip;
