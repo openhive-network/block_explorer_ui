4148d4073853f652b6e9aab535954d5cf37fa8b9
jlachor <jlachor@syncad.com>
2025-02-20 11:30:23 +0100

    Start writting service for HC

---
+++ services/HealthCheckerService.ts
@@ -0,0 +1,122 @@
+import { HealthChecker, IHiveChainInterface, TScoredEndpoint } from "@hiveio/wax";
+
+export interface ApiChecker {
+  title: string;
+  method: any;
+  params: any;
+  validatorFunction: (data: any) => string | true;
+}
+
+const LOCAL_PROVIDERS = "localProviders";
+const FALLBACKS = "fallbacks";
+
+class HealthCheckerService {
+
+  private hiveChain?: IHiveChainInterface;
+  private apiCheckers?: ApiChecker[];
+  private defaultProviders?: string[];
+  private providers?: string[];
+  private fallbacks?: string[];
+  private scoredEndpoints?: TScoredEndpoint[];
+  private healthChecker?: HealthChecker;
+  private nodeAddress: string | null = null;
+  private setNodeAddress: (node:string) => void = () => {}
+
+  constructor(
+    hiveChain: IHiveChainInterface,
+    apiCheckers: ApiChecker[],
+    defaultProviders: string[],
+    healthChecker: HealthChecker,
+    nodeAddress: string | null,
+    setNodeAddress: (node: string) => void,
+  ) {
+    this.hiveChain = hiveChain;
+    this.healthChecker = healthChecker;
+    this.apiCheckers = apiCheckers;
+    this.readFallbacksFromLocalStorage();
+    this.readLocalProvidersFromLocalStorage();
+    this.nodeAddress = nodeAddress;
+    this.setNodeAddress = setNodeAddress;
+    this.defaultProviders = defaultProviders;
+  }
+
+
+  // Local Storage
+
+  readLocalProvidersFromLocalStorage = () => {
+    try {
+      const readValue = window.localStorage.getItem(LOCAL_PROVIDERS);
+      if (readValue) { 
+        this.providers = JSON.parse(readValue);
+      } else {
+        this.providers = this.defaultProviders;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  readFallbacksFromLocalStorage = () => {
+    try {
+      const readValue = window.localStorage.getItem(FALLBACKS);
+      if (readValue) { 
+        this.fallbacks = JSON.parse(readValue);
+      } 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  writeLocalProvidersToLocalStorage = async (localProviders: string[]) => {
+    try {
+      if (localProviders && localProviders.length > 0) {
+        await window.localStorage.setItem(LOCAL_PROVIDERS, JSON.stringify(localProviders));
+        this.providers = localProviders;
+      } else {
+        await window.localStorage.removeItem(LOCAL_PROVIDERS);
+        this.providers = undefined;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  writeFallbacksToLocalStorage = async (fallbacks: string[]) => {
+    try {
+      if (fallbacks && fallbacks.length > 0) {
+        await window.localStorage.setItem(FALLBACKS, JSON.stringify(fallbacks));
+        this.fallbacks = fallbacks;
+      } else {
+        await window.localStorage.removeItem(FALLBACKS);
+        this.fallbacks = undefined;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // HC Logic
+
+  createHealthChecker = async () => {
+    this.healthChecker?.on('error', error => console.error(error.message));
+    this.healthChecker?.on("data", (data: Array<TScoredEndpoint>) => { 
+      console.log(JSON.stringify(data)); 
+      this.checkForFallbacks(data); 
+      if (data.length)this.scoredEndpoints = data
+    });
+    // this.healthChecker?.on("validationerror", error => markValidationError(error.apiEndpoint.id, error.request.endpoint, error));
+  }
+
+  checkForFallbacks = (scoredEndpoints: TScoredEndpoint[]) => {
+    const currentScoredEndpoint = scoredEndpoints.find((scoredEdnpoint) => scoredEdnpoint.endpointUrl === this.nodeAddress);
+    if (currentScoredEndpoint && !currentScoredEndpoint.up) {
+      this.fallbacks?.forEach((fallback) => {
+        const fallbackScoredEndpoint = scoredEndpoints.find((scoredEdnpoint) => scoredEdnpoint.endpointUrl === fallback);
+        if (fallbackScoredEndpoint && fallbackScoredEndpoint.up) this.setNodeAddress(fallback) ;
+      })
+    }
+  }
+
+}
+
+export default HealthCheckerService;
