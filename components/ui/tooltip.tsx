98f105f5d8b1c43b812c0c2d8b3bbd14c904d6ba
Dima Rifai <dima.rifai@gmail.com>
2025-02-25 08:56:44 +0200

    Issue #465 - Style update on tooltip content

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -20,7 +20,7 @@ const TooltipContent = React.forwardRef<
     sideOffset={sideOffset}
     className={cn(
       "z-50 overflow-hidden rounded-[6px] bg-theme text-text shadow-sm animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-      "border border-theme/70 py-1 px-2 text-xs font-medium",
+      "border border-theme/70 py-1 px-1 text-xs font-medium",
       className
     )}
     {...props}
