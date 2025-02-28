93ae5eb51222473ca7489e8c08be034599707f75
Lukas <lukas.budginas@gmail.com>
2025-02-28 19:00:51 +0200

    Make interactions tab filter section collapsible

--- components/account/tabs/interactions/InteractionsTabContent.tsx
+++ components/account/tabs/interactions/InteractionsTabContent.tsx
@@ -1,8 +1,9 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { Loader2 } from "lucide-react";
+import { ArrowDown, ArrowUp, Loader2 } from "lucide-react";
 
 import useCommentSearch from "@/hooks/api/common/useCommentSearch";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
 import { TabsContent } from "@/components/ui/tabs";
 import AccountPageInteractionSearch from "./AccountPageInteractionSearch";
 import NoResult from "@/components/NoResult";
@@ -13,6 +14,8 @@ import { DEFAULT_PARAMS } from "./AccountPageInteractionSearch";
 
 const InteractionsTabContent = () => {
   const router = useRouter();
+  const [isCardContentHidden, setIsCardContentHidden] = useState(true);
+
   const accountNameFromRoute = (router.query.accountName as string)?.replace(
     "@",
     ""
@@ -49,13 +52,40 @@ const InteractionsTabContent = () => {
     }
   };
 
+  const handleCardContentVisibility = () => {
+    setIsCardContentHidden(!isCardContentHidden);
+  };
+
+  // Don't show filters section if only account name and permlink are present
+  useEffect(() => {
+    Object.keys(DEFAULT_PARAMS).forEach((key) => {
+      if (key === "accountName" || key === "permlink") return;
+
+      const param = paramsState[key as keyof typeof DEFAULT_PARAMS];
+      const defaultParam = DEFAULT_PARAMS[key as keyof typeof DEFAULT_PARAMS];
+      if (param !== defaultParam) {
+        setIsCardContentHidden(false);
+      }
+    });
+  }, [paramsState]);
+
   return (
     <TabsContent value="interactions">
       <Card className="mb-4">
-        <CardHeader>
-          <CardTitle>Interaction Search</CardTitle>
+        <CardHeader
+          className="p-0 m-0"
+          onClick={handleCardContentVisibility}
+        >
+          <div className="flex justify-center  items-center p-3 hover:bg-rowHover cursor-pointer">
+            <div className="w-full text-center text-2xl">
+              Interaction Search
+            </div>
+            <div className="flex justify-end items-center">
+              {isCardContentHidden ? <ArrowDown /> : <ArrowUp />}
+            </div>
+          </div>
         </CardHeader>
-        <CardContent>
+        <CardContent hidden={isCardContentHidden}>
           <AccountPageInteractionSearch
             isCommentSearchDataLoading={isCommentSearchDataLoading}
           />
