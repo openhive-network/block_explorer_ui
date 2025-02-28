af634d589d7d8fc2d7156e1c4d63177b900314e8
jlachor <jlachor@syncad.com>
2025-02-28 13:56:09 +0100

    Add lit for testing to project

---
+++ components/healthchecker/healthchecker-wc.ts
@@ -0,0 +1,19 @@
+import {LitElement, html} from 'lit';
+import {customElement} from 'lit/decorators.js';
+
+@customElement('health-checker-webcomponent')
+class HealthCheckerWebcomponent extends LitElement {
+
+  render() {
+
+    return html`
+
+      <div>HC</div>
+
+    `;
+
+  }
+
+}
+
+export default HealthCheckerWebcomponent
