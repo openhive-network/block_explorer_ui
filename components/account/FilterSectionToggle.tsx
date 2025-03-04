26a1fc2e536fce7cba4b5079e5aceb6553ea8a92
Lukas <lukas.budginas@gmail.com>
2025-03-04 16:55:47 +0200

    Create account filter section toggle

---
+++ components/account/FilterSectionToggle.tsx
@@ -0,0 +1,28 @@
+import { Filter } from "lucide-react";
+import { Button } from "../ui/button";
+
+interface FilterSectionToggleProps {
+  toggleFilters: () => void;
+  isFiltersActive: boolean;
+}
+
+const FilterSectionToggle: React.FC<FilterSectionToggleProps> = ({
+  toggleFilters,
+  isFiltersActive,
+}) => {
+  return (
+    <span className="relative">
+      {isFiltersActive ? (
+        <span className="absolute -top-5 -right-1 flex size-3">
+          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+          <span className="relative inline-flex size-3 rounded-full bg-green-500"></span>
+        </span>
+      ) : null}
+      <Button onClick={toggleFilters}>
+        <Filter />
+      </Button>
+    </span>
+  );
+};
+
+export default FilterSectionToggle;
