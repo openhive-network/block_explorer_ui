7584b6d5b919a5a467333fde90c6d0f11809fdc3
Lukas <lukas.budginas@gmail.com>
2025-03-05 19:31:41 +0200

    Update hover styles for account tabs and post type selector

--- components/account/tabs/AccountOperationViewTabs.tsx
+++ components/account/tabs/AccountOperationViewTabs.tsx
@@ -39,19 +39,19 @@ const AccountOperationViewTabs: React.FC<AccountOperationViewTabs> = ({
       <TabsList className="flex w-full justify-start">
         <div className="bg-theme p-1 flex gap-2 rounded w-auto">
           <TabsTrigger
-            className="rounded"
+            className="rounded cursor-pointer hover:bg-buttonHover"
             value="operations"
           >
             Operations
           </TabsTrigger>
           <TabsTrigger
-            className="rounded"
+            className="rounded cursor-pointer hover:bg-buttonHover"
             value="comments"
           >
             Comments
           </TabsTrigger>
           <TabsTrigger
-            className="rounded"
+            className="rounded cursor-pointer hover:bg-buttonHover"
             value="interactions"
           >
             Interactions
