e2c7c5d94485fd8043289e233346c5f95649e765
Lukas <lukas.budginas@gmail.com>
2025-03-07 20:11:16 +0200

    Add on key down event on jump to page component

--- components/JumpToPage.tsx
+++ components/JumpToPage.tsx
@@ -29,15 +29,15 @@ const JumpToPage = ({
     const inputValueNumber = Number(inputValue);
 
     if (inputValue === "") {
-        // Handle the case where the input is empty (e.g., reset to current page)
-        setValue(currentPage);
-        setInputValue(String(currentPage));
+      // Handle the case where the input is empty (e.g., reset to current page)
+      setValue(currentPage);
+      setInputValue(String(currentPage));
     } else if (inputValueNumber >= 1 && inputValueNumber <= totalPageCount) {
-        setValue(inputValueNumber);
+      setValue(inputValueNumber);
     } else {
-        // Reset the input value and state to the current page
-        setValue(currentPage);
-        setInputValue(String(currentPage));
+      // Reset the input value and state to the current page
+      setValue(currentPage);
+      setInputValue(String(currentPage));
     }
   };
 
@@ -46,15 +46,21 @@ const JumpToPage = ({
     onPageChange(value);
   };
 
+  const handleOnKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+      handleBlur();
+      onPageChange(value);
+    }
+  };
+
   useEffect(() => {
     setValue(currentPage);
     setInputValue(String(currentPage));
   }, [currentPage]);
-  
+
   if (totalPageCount <= 1) {
     return null;
   }
-
 
   return (
     <form
@@ -68,6 +74,7 @@ const JumpToPage = ({
         max={totalPageCount}
         onChange={onInputChange}
         onBlur={handleBlur}
+        onKeyDown={handleOnKeyDown}
         className="w-0 min-w-[72px] p-1 pl-2 py-2 text-gray-900 border border-gray-300 rounded sm:text-xs bg-gray-50 focus:bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-theme dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:focus:bg-gray-700"
         data-testid="input-goto-page"
       />
