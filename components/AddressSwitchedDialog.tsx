20482a91c3315dd41c03de0e10e6941e80925ebf
Dima Rifai <dima.rifai@gmail.com>
2025-03-28 22:15:13 +0200

    Issue #500 - Add list of nodes and handle them

--- components/AddressSwitchedDialog.tsx
+++ components/AddressSwitchedDialog.tsx
@@ -8,8 +8,9 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { config } from "@/Config";
-import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
+import { HiveNodes } from "@/utils/HiveNodes";
+import { Input } from "@/components/ui/input";
 
 type AddressSwitcherDialogProps = {
   addressType: "node" | "api";
@@ -23,14 +24,8 @@ const AddressSwitchedDialog: React.FC<AddressSwitcherDialogProps> = ({
   currentAddress,
 }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
   const [userAddress, setUserAddress] = useState<string>("");
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter" && isOpen) {
-      onSubmitClick();
-    }
-  };
+  const [customNodeAddress, setCustomNodeAddress] = useState<string>("");
 
   const getDefaultApiAddress = (): string => {
     if (addressType === "api") {
@@ -41,7 +36,7 @@ const AddressSwitchedDialog: React.FC<AddressSwitcherDialogProps> = ({
   };
 
   const onSubmitClick = () => {
-    setAddress(userAddress);
+    setAddress(userAddress || customNodeAddress);
     setIsOpen(false);
   };
 
@@ -53,6 +48,15 @@ const AddressSwitchedDialog: React.FC<AddressSwitcherDialogProps> = ({
   useEffect(() => {
     setUserAddress(currentAddress || "");
   }, [currentAddress]);
+
+  const handleNodeSelect = (node: string) => {
+    setUserAddress(node);
+    setCustomNodeAddress("");
+  };
+
+  const removeProtocol = (url: string): string => {
+    return url.replace(/^https?:\/\//, '');
+  };
 
   return (
     <Dialog
@@ -70,40 +74,79 @@ const AddressSwitchedDialog: React.FC<AddressSwitcherDialogProps> = ({
         </div>
       </DialogTrigger>
       <DialogContent
-        onKeyDown={handleKeyDown}
-        className="h-1/4 max-w-3xl overflow-auto bg-white  dark:bg-theme dark:text-white"
+        className="h-[70vh] max-w-3xl overflow-auto flex flex-col rounded-lg shadow-md border"
         data-testid="api-address-dialog"
       >
-        <DialogHeader>
-          <DialogTitle data-testid="api-address-header-title">
+        <DialogHeader className="px-2 pt-2">
+          <DialogTitle data-testid="api-address-header-title" className="text-lg font-semibold">
             {addressType === "api"
               ? "Block Explorer backend API address"
               : "Hive node address"}
           </DialogTitle>
         </DialogHeader>
-        <Input
-          autoFocus={true}
-          className="focus:bg-white dark:focus:bg-gray-700"
-          type="text"
-          data-testid="api-address-input"
-          value={userAddress}
-          onChange={(e) => setUserAddress(e.target.value)}
-        />
-        <DialogFooter>
+
+        <div className="px-6 py-2">
+          <div className="space-y-4">
+            <div>
+              <p className="font-semibold">Available Nodes:</p>
+              <p className="text-sm text-gray-500 dark:text-gray-500 mb-2">Select from the list of available nodes.</p>
+
+              <div className="space-y-2">
+                {HiveNodes.map((node) => (
+                  <div key={node} className="flex items-start space-x-2">
+                    <input
+                      type="radio"
+                      id={node}
+                      name="hive_node"
+                      value={node}
+                      checked={userAddress === node}
+                      onChange={() => handleNodeSelect(node)}
+                      className="cursor-pointer mt-3" 
+                    />
+                    <div className="flex flex-col">
+                      <label htmlFor={node} className="cursor-pointer font-medium text-explorer-dark-gray dark:text-white">
+                        {removeProtocol(node)}
+                      </label>
+                      <p className="text-xs text-gray-500">{node}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div>
+              <p className="font-semibold text-explorer-dark-gray dark:text-white ">Add Custom Node:</p>
+              <p className="text-sm text-gray-500 dark:text-gray-500 mb-2">Enter a custom Hive node URL.</p>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="text"
+                  placeholder="https://"
+                  value={customNodeAddress}
+                  onChange={(e) => {
+                    setCustomNodeAddress(e.target.value);
+                    setUserAddress("")
+                  }}
+                  className="w-full border rounded-md focus:ring-blue-500 focus:border-blue-500 dark:focus:bg-gray-700"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <DialogFooter className="mt-auto px-6 py-4">
           <div className="w-full flex justify-between">
             <Button
               type="button"
-              // variant="secondary"
-              // className="rounded"
               data-testid="api-address-reset-button"
               onClick={onResetClick}
+              variant="outline"
             >
-              Reset to default
+              Restore Defaults
             </Button>
             <Button
-              // className="rounded"
               data-testid="api-address-submit-button"
               onClick={onSubmitClick}
+              disabled={!userAddress && !customNodeAddress}
             >
               Submit
             </Button>
