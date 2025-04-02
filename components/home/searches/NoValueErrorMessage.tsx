ea68e612914c7d3a8eea6a716d91e9a7665566e5
Lukas <lukas.budginas@gmail.com>
2025-04-02 19:14:57 +0300

    Create no value error mesage for main page searches section

---
+++ components/home/searches/NoValueErrorMessage.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface NoValueErrorMessage {
+  accountName: string;
+  isSearchButtonDisabled: boolean;
+}
+
+const NoValueErrorMessage: React.FC<NoValueErrorMessage> = ({
+  accountName,
+  isSearchButtonDisabled,
+}) => {
+  let message = "";
+
+  if (!accountName && isSearchButtonDisabled) {
+    message = "Please insert required values";
+  } else if (!accountName) {
+    message = "Set account name";
+  } else if (isSearchButtonDisabled) {
+    message = `Value field can't be empty`;
+  } else {
+    message = "";
+  }
+
+  if (!message) return;
+
+  return (
+    <label className="ml-2 text-gray-300 dark:text-gray-500 ">{message}</label>
+  );
+};
+
+export default NoValueErrorMessage;
