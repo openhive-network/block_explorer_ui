87ecfbd24e7f1da3f29fbb39142c56ec09191324
jlachor <jlachor@syncad.com>
2025-04-02 12:55:12 +0200

    Set necessary providers, context and service for HC

--- Config.ts
+++ Config.ts
@@ -42,4 +42,20 @@ export const config = {
     outgoing: "outgoing",
   },
   maxProxyDepth: 3,
+  defaultProviders: [
+    "https://api.hive.blog",
+    "https://api.openhive.network",
+    "https://anyx.io",
+    "https://rpc.ausbit.dev",
+    "https://rpc.mahdiyari.info",
+    "https://techcoderx.com",
+    "https://hive.roelandp.nl",
+    "https://hived.emre.sh",
+    "https://api.deathwing.me",
+    "https://api.c0ff33a.uk",
+    "https://hive-api.arcange.eu",
+    "https://hive-api.3speak.tv",
+    "https://hiveapi.actifit.io"
+  ]
+
 };
--- contexts/HealthCheckerContext.tsx
+++ contexts/HealthCheckerContext.tsx
@@ -4,6 +4,8 @@ import { useAddressesContext } from "./AddressesContext";
 // import HealthCheckerService, { HealthCheckerFields } from "@/services/HealthCheckerService";
 import {HealthCheckerService, ApiChecker} from "@hiveio/healthchecker-component";
 import { useHiveChainContext } from "./HiveChainContext";
+import { config } from "@/Config";
+import { ExplorerNodeApi } from "@/services/FetchingService";
 
 
 type HealthCheckerContextType = {
@@ -28,15 +30,60 @@ export const useHealthCheckerContext = () => {
 };
 
 export const HealthCheckerContextProvider: React.FC<{
-  apiCheckers: ApiChecker[];
-  defaultProviders: string[];
-  nodeAddress: string | null;
-  setNodeAddress: (node: string) => void;
   children: React.ReactNode;
-}> = ({ apiCheckers, defaultProviders, children }) => {
+}> = ({ children }) => {
 
+  const defaultProviders = config.defaultProviders;
+
+  
+  
+  
   const {nodeAddress, setNodeAddress} = useAddressesContext();
   const {hiveChain} = useHiveChainContext();
+  if (!hiveChain) return null;
+  const extendedHiveChain = hiveChain
+  ?.extend<ExplorerNodeApi>();
+
+
+  const apiCheckers: ApiChecker[] = [
+    {
+      title: "Reward Funds",
+      method: extendedHiveChain?.api.database_api.get_reward_funds,
+      params: {}, 
+      validatorFunction: data => !!data.funds ? true : "Reward funds error",
+    },
+    {
+      title: "Dynamic Global",
+      method: extendedHiveChain?.api.database_api.get_dynamic_global_properties,
+      params: {}, 
+      validatorFunction: data => data.id === 0 ? true : "Dynamic global error",
+    },
+    {
+      title: "Price Feed",
+      method: extendedHiveChain?.api.database_api.get_current_price_feed,
+      params: {}, 
+      validatorFunction: data => !!data.base ? true : "Price feed error",
+    },
+    {
+      title: "Witness Schedule",
+      method: extendedHiveChain?.api.database_api.get_witness_schedule,
+      params: { id: 1 }, 
+      validatorFunction: data => /*data.max_scheduled_witnesses === 21*/ !!data ? true : "Witness schedule error",
+      // This is left wrong on purpose for tests
+    },
+    {
+      title: "Vesting Delegations",
+      method: extendedHiveChain?.api.database_api.find_vesting_delegations,
+      params: { account: "hiveio" }, 
+      validatorFunction: data => !!data.delegations ? true : "Vesting delegations error",
+    },
+    {
+      title: "RC Direct Delegations",
+      method: extendedHiveChain?.api.rc_api.list_rc_direct_delegations,
+      params: { start: ["hiveio", ""], limit: 1000 }, 
+      validatorFunction: data => !!data.rc_direct_delegations ? true : "RC delegation error",
+    }
+  ]
 
   const [healthCheckerService, setHealthCheckerService] = useState<HealthCheckerService | undefined>(undefined);
 
