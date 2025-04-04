4bcfc81e42d14e0ad5123e0e6c309e1dd86a9774
jlachor <jlachor@syncad.com>
2025-04-04 13:33:51 +0200

    Properly configure tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./node_modules/healthchecker-component/**/*.{ts,tsx,js,jsx}"
   ],
   theme: {
     container: {
