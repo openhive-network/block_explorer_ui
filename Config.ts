380a2033bafa8b1459debc3a6decd98739e96c01
Lukas <lukas.budginas@gmail.com>
2025-04-07 13:57:25 +0300

    Add market history chart refresh interval

--- Config.ts
+++ Config.ts
@@ -28,6 +28,7 @@ export const config = {
   maxDelegatorsCount: 1000,
   mainRefreshInterval: 3000,
   accountRefreshInterval: 20000,
+  marketHistoryRefreshInterval: 60000,
   lastBlocksForWidget: 20,
   firstBlockTime: "2016-03-24T16:05:00",
   precisions: {
