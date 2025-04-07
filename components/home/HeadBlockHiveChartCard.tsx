c73acb2e189ff71c04744af555fca53a449e80be
Lukas <lukas.budginas@gmail.com>
2025-04-07 14:40:39 +0300

    Update market history chart with custom interval

--- components/home/HeadBlockHiveChartCard.tsx
+++ components/home/HeadBlockHiveChartCard.tsx
@@ -2,6 +2,9 @@ import { ArrowDown, ArrowUp } from "lucide-react";
 import MarketHistoryChart from "./MarketHistoryChart";
 import moment from "moment";
 import useMarketHistory from "@/hooks/common/useMarketHistory";
+import { useEffect, useState } from "react";
+import { useUserSettingsContext } from "@/contexts/UserSettingsContext";
+import { config } from "@/Config";
 
 interface HeadBlockPropertyCardProps {
   header: string;
@@ -10,23 +13,45 @@ interface HeadBlockPropertyCardProps {
 }
 
 const MARKET_HISTORY_INTERVAL = 86400; // 1 day
-const CURRENT_TIME = moment().format("YYYY-MM-DDTHH:mm:ss");
-const MARKET_HISTORY_TIME_PERIOD = moment()
-  .subtract(30, "days")
-  .format("YYYY-MM-DDTHH:mm:ss");
+const CHART_UPDATE_INTERVAL = config.marketHistoryRefreshInterval;
 
 const HeadBlockHiveChartCard: React.FC<HeadBlockPropertyCardProps> = ({
   header,
   isParamsHidden,
   handleHideParams,
 }) => {
+  const { settings } = useUserSettingsContext();
+
+  const CURRENT_TIME = moment().format("YYYY-MM-DDTHH:mm:ss");
+  const MARKET_HISTORY_TIME_PERIOD = moment()
+    .subtract(30, "days")
+    .format("YYYY-MM-DDTHH:mm:ss");
+
+  const [marketHistoryStartDate, setMarketHistoryStartDate] = useState<string>(
+    MARKET_HISTORY_TIME_PERIOD
+  );
+  const [marketHistoryEndDate, setMarketHistoryEndDate] =
+    useState<string>(CURRENT_TIME);
+
   const { marketHistory } = useMarketHistory(
     MARKET_HISTORY_INTERVAL,
-    MARKET_HISTORY_TIME_PERIOD,
-    CURRENT_TIME
+    marketHistoryStartDate,
+    marketHistoryEndDate
   );
 
-  if (!marketHistory) return null;
+  // Hive chart market data updates
+  useEffect(() => {
+    if (!settings.liveData) return;
+
+    const intervalId = setInterval(() => {
+      setMarketHistoryStartDate(
+        moment().subtract(30, "days").format("YYYY-MM-DDTHH:mm:ss")
+      );
+      setMarketHistoryEndDate(moment().format("YYYY-MM-DDTHH:mm:ss"));
+    }, CHART_UPDATE_INTERVAL);
+
+    return () => clearInterval(intervalId);
+  }, [settings.liveData]);
 
   return (
     <div
