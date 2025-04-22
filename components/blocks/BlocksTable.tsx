40cdaf2e90f134630d18f304616a6f58b2237745
Dima Rifai <dima.rifai@gmail.com>
2025-04-22 20:21:22 +0300

    Issue #533 - Make edges of pagination rounded

--- components/blocks/BlocksTable.tsx
+++ components/blocks/BlocksTable.tsx
@@ -212,13 +212,14 @@ const BlocksTable: React.FC<BlocksTableProps> = ({
     <>
       {totalCount > config.standardPaginationSize ? (
         <div className="sticky z-20 top-[3.2rem] md:top-[4rem] mt-6 bg-theme rounded">
-          <div className="flex flex-col md:flex-row items-center gap-2 flex-1 justify-between w-full ">
+          <div className="flex flex-col md:flex-row items-center gap-2 flex-1 justify-between w-full rounded ">
             <CustomPagination
               currentPage={currentPage || 1}
               totalCount={totalCount}
               pageSize={config.standardPaginationSize}
               onPageChange={onPageChange}
               isMirrored={true}
+              className="rounded"
             />
             <div className="flex items-center mt-2 w-full md:w-auto justify-center md:justify-end mb-2">
               <JumpToPage
