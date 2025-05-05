ddf8aacc1b8ff93b4de53f8e7b105a109784b3fa
Lukas <lukas.budginas@gmail.com>
2025-05-05 22:47:25 +0300

    Remember user selection for balance history page filter section visibility when filters are active

--- components/home/searches/BalanceHistorySearch.tsx
+++ components/home/searches/BalanceHistorySearch.tsx
@@ -1,157 +1,110 @@
+// *** Operations related code is commented out now ***
+// TODO: Remove code in future if it won't be used
+
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import SearchRanges from "@/components/searchRanges/SearchRanges";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-import useURLParams from "@/hooks/common/useURLParams";
-import OperationTypesDialog from "@/components/OperationTypesDialog";
-import useAccountOperationTypes from "@/hooks/api/accountPage/useAccountOperationTypes";
+// import useURLParams from "@/hooks/common/useURLParams";
+// import OperationTypesDialog from "@/components/OperationTypesDialog";
+// import useAccountOperationTypes from "@/hooks/api/accountPage/useAccountOperationTypes";
 import { useSearchesContext } from "@/contexts/SearchesContext";
 import { cn } from "@/lib/utils";
-
-interface AccountSearchParams {
-  accountName?: string | undefined;
-  fromBlock: number | undefined;
-  toBlock: number | undefined;
-  fromDate: Date | undefined;
-  toDate: Date | undefined;
-  lastBlocks: number | undefined;
-  lastTime: number | undefined;
-  timeUnit: string | undefined;
-  rangeSelectKey: string | undefined;
-  page: number | undefined;
-  filters: boolean[] | undefined;
-  coinType?: string;
-}
-
-const defaultSearchParams: AccountSearchParams = {
-  accountName: undefined,
-  fromBlock: undefined,
-  toBlock: undefined,
-  fromDate: undefined,
-  toDate: undefined,
-  lastBlocks: undefined,
-  lastTime: 30,
-  timeUnit: "days",
-  rangeSelectKey: "none",
-  page: undefined,
-  filters: undefined,
-  coinType: "HIVE", // Default to HIVE
-};
+import { removeStorageItem, getLocalStorage } from "@/utils/LocalStorage";
+import { defaultBalanceHistorySearchParams } from "@/pages/balanceHistory/[accountName]";
 
 const COIN_TYPES = ["HIVE", "VESTS", "HBD"];
+const DEFAULT_COIN_TYPE = "HIVE";
 
 const BalanceHistorySearch = ({
   paramsState,
   setParams,
-  isBalanceFilterSectionVisible,
+  isVisible,
+  setIsVisible,
   setIsFiltersActive,
+  isFiltersActive,
 }: any) => {
   const [coinType, setCoinType] = useState<string>(
-    paramsState.coinType ?? "HIVE"
+    paramsState.coinType ?? DEFAULT_COIN_TYPE
   ); // State to store the selected coin name
   const router = useRouter();
   const { searchRanges } = useSearchesContext();
   const accountNameFromRoute = (router.query.accountName as string)?.slice(1);
-  const { accountOperationTypes } =
-    useAccountOperationTypes(accountNameFromRoute);
-  const [selectedOperationTypes, setSelectedOperationTypes] = useState<
-    number[]
-  >([]);
-  const [singleOperationTypeId, setSingleOperationTypeId] = useState<
-    number | undefined
-  >(undefined);
-  const [fieldContent, setFieldContent] = useState<string>("");
-  const [selectedKeys, setSelectedKeys] = useState<string[] | undefined>(
-    undefined
-  );
-  const [selectedIndex, setSelectedIndex] = useState<string>("");
+  // const { accountOperationTypes } =
+  //   useAccountOperationTypes(accountNameFromRoute);
+  // const [selectedOperationTypes, setSelectedOperationTypes] = useState<
+  //   number[]
+  // >([]);
+  // const [singleOperationTypeId, setSingleOperationTypeId] = useState<
+  //   number | undefined
+  // >(undefined);
+  // const [fieldContent, setFieldContent] = useState<string>("");
+  // const [selectedKeys, setSelectedKeys] = useState<string[] | undefined>(
+  //   undefined
+  // );
+  // const [selectedIndex, setSelectedIndex] = useState<string>("");
 
-  const changeSelectedOperationTypes = (operationTypesIds: number[]) => {
-    if (operationTypesIds.length === 1) {
-      setSingleOperationTypeId(operationTypesIds[0]);
-    } else {
-      setSingleOperationTypeId(undefined);
-    }
-    setSelectedKeys(undefined);
-    setFieldContent("");
-    setSelectedOperationTypes(operationTypesIds);
-  };
+  // const changeSelectedOperationTypes = (operationTypesIds: number[]) => {
+  //   if (operationTypesIds.length === 1) {
+  //     setSingleOperationTypeId(operationTypesIds[0]);
+  //   } else {
+  //     setSingleOperationTypeId(undefined);
+  //   }
+  //   setSelectedKeys(undefined);
+  //   setFieldContent("");
+  //   setSelectedOperationTypes(operationTypesIds);
+  // };
 
-  const {
-    filters: filtersParam,
-    fromBlock: fromBlockParam,
-    toBlock: toBlockParam,
-    fromDate: fromDateParam,
-    toDate: toDateParam,
-    lastBlocks: lastBlocksParam,
-    timeUnit: timeUnitParam,
-    lastTime: lastTimeParam,
-    rangeSelectKey,
-    page,
-  } = paramsState;
+  // const {
+  //   filters: filtersParam,
+  //   fromBlock: fromBlockParam,
+  //   toBlock: toBlockParam,
+  //   fromDate: fromDateParam,
+  //   toDate: toDateParam,
+  //   lastBlocks: lastBlocksParam,
+  //   timeUnit: timeUnitParam,
+  //   lastTime: lastTimeParam,
+  //   rangeSelectKey,
+  //   page,
+  // } = paramsState;
 
-  const [initialSearch, setInitialSearch] = useState<boolean>(false);
-  const [filters, setFilters] = useState<boolean[] | undefined>(undefined);
+  // const [initialSearch, setInitialSearch] = useState<boolean>(false);
+  // const [filters, setFilters] = useState<boolean[] | undefined>(undefined);
   const [isSearchButtonDisabled, setIsSearchButtonDisabled] = useState(false);
 
-  const handleSearch = async (resetPage?: boolean) => {
-    if (
-      !initialSearch &&
-      (!!fromDateParam ||
-        !!toDateParam ||
-        !!fromBlockParam ||
-        !!toBlockParam ||
-        !!lastBlocksParam ||
-        !!lastTimeParam ||
-        !!filtersParam?.length)
-    ) {
-      fromDateParam && searchRanges.setStartDate(fromDateParam);
-      toDateParam && searchRanges.setEndDate(toDateParam);
-      fromBlockParam && searchRanges.setFromBlock(fromBlockParam);
-      toBlockParam && searchRanges.setToBlock(toBlockParam);
-      lastBlocksParam && searchRanges.setLastBlocksValue(lastBlocksParam);
-      timeUnitParam && searchRanges.setTimeUnitSelectKey(timeUnitParam);
-      rangeSelectKey && searchRanges.setRangeSelectKey(rangeSelectKey);
-      searchRanges.setLastTimeUnitValue(lastTimeParam);
-      setFilters(filtersParam);
-      setInitialSearch(true);
-    } else {
-      if (!initialSearch) {
-        setInitialSearch(true);
-      }
+  const handleSearch = async () => {
+    const {
+      payloadFromBlock,
+      payloadToBlock,
+      payloadStartDate,
+      payloadEndDate,
+    } = await searchRanges.getRangesValues();
 
-      const {
-        payloadFromBlock,
-        payloadToBlock,
-        payloadStartDate,
-        payloadEndDate,
-      } = await searchRanges.getRangesValues();
+    const props = {
+      ...paramsState,
+      fromBlock: payloadFromBlock,
+      toBlock: payloadToBlock,
+      fromDate: payloadStartDate,
+      toDate: payloadEndDate,
+      lastBlocks:
+        searchRanges.rangeSelectKey === "lastBlocks"
+          ? searchRanges.lastBlocksValue
+          : undefined,
+      lastTime:
+        searchRanges.rangeSelectKey === "lastTime"
+          ? searchRanges.lastTimeUnitValue
+          : undefined,
+      timeUnit:
+        searchRanges.rangeSelectKey === "lastTime"
+          ? searchRanges.timeUnitSelectKey
+          : undefined,
+      rangeSelectKey: searchRanges.rangeSelectKey,
+      page: undefined,
+    };
 
-      setParams({
-        ...paramsState,
-        filters: filters,
-        fromBlock: payloadFromBlock,
-        toBlock: payloadToBlock,
-        fromDate: payloadStartDate,
-        toDate: payloadEndDate,
-        lastBlocks:
-          searchRanges.rangeSelectKey === "lastBlocks"
-            ? searchRanges.lastBlocksValue
-            : undefined,
-        lastTime:
-          searchRanges.rangeSelectKey === "lastTime"
-            ? searchRanges.lastTimeUnitValue
-            : undefined,
-        timeUnit:
-          searchRanges.rangeSelectKey === "lastTime"
-            ? searchRanges.timeUnitSelectKey
-            : undefined,
-        rangeSelectKey: searchRanges.rangeSelectKey,
-        page: resetPage ? undefined : page,
-      });
-    }
+    setParams(props);
   };
 
   const handleCoinTypeChange = (newCoinType: string) => {
@@ -164,47 +117,57 @@ const BalanceHistorySearch = ({
   };
 
   const handleFilterClear = () => {
-    const newPage = rangeSelectKey !== "none" ? undefined : page;
-    setParams({
-      ...defaultSearchParams,
+    const {
+      setRangeSelectKey,
+      setTimeUnitSelectKey,
+      setLastTimeUnitValue,
+      setLastBlocksValue,
+    } = searchRanges;
+
+    const props = {
+      ...defaultBalanceHistorySearchParams,
       accountName: accountNameFromRoute,
-      page: newPage,
-      lastTime: undefined,
-      rangeSelectKey: "none",
-      timeUnit: undefined,
-    });
+      coinType: DEFAULT_COIN_TYPE,
+    };
+
+    setParams(props);
+
+    setRangeSelectKey("none");
+    setTimeUnitSelectKey("days");
+    setLastTimeUnitValue(30);
+    setLastBlocksValue(1000);
+    setCoinType(DEFAULT_COIN_TYPE);
+
+    setIsVisible(false);
     setIsFiltersActive(false);
-    searchRanges.setRangeSelectKey("none");
-    searchRanges.setTimeUnitSelectKey("days");
-    searchRanges.setLastTimeUnitValue(undefined);
-    setFilters([]);
+
+    removeStorageItem("is_balance_filters_visible");
   };
 
   useEffect(() => {
-    if (paramsState.coinType) {
-      setCoinType(paramsState.coinType);
-    }
-    if (paramsState && !initialSearch) {
-      handleSearch();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [paramsState]);
-
-  useEffect(() => {
-    let filtersActive = false;
-    Object.keys(defaultSearchParams).forEach((key) => {
+    Object.keys(defaultBalanceHistorySearchParams).forEach((key) => {
       if (key === "accountName" || key === "page") return;
 
-      const param = paramsState[key as keyof typeof defaultSearchParams];
+      const param =
+        paramsState[key as keyof typeof defaultBalanceHistorySearchParams];
       const defaultParam =
-        defaultSearchParams[key as keyof typeof defaultSearchParams];
+        defaultBalanceHistorySearchParams[
+          key as keyof typeof defaultBalanceHistorySearchParams
+        ];
+
+      const visibleFilters =
+        (isFiltersActive &&
+          getLocalStorage("is_balance_filters_visible", true)) ??
+        true;
 
       if (param !== defaultParam) {
-        filtersActive = true;
+        setIsFiltersActive(true);
+        setIsVisible(visibleFilters);
       }
     });
-    setIsFiltersActive(filtersActive);
-  }, [paramsState, setIsFiltersActive]);
+
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [paramsState, isFiltersActive]);
 
   const buttonLabel = `Value field can't be empty`;
 
@@ -214,7 +177,7 @@ const BalanceHistorySearch = ({
         className={cn(
           "mb-4 overflow-hidden transition-all duration-500 ease-in max-h-0 opacity-0 mt-4",
           {
-            "max-h-full opacity-100": isBalanceFilterSectionVisible,
+            "max-h-full opacity-100": isVisible,
           }
         )}
       >
@@ -242,7 +205,7 @@ const BalanceHistorySearch = ({
             rangesProps={searchRanges}
             setIsSearchButtonDisabled={setIsSearchButtonDisabled}
           />
-          {/* Operations Types commented for now
+          {/* 
           <div className="flex items-center mb-10 mt-2">
         <OperationTypesDialog
           operationTypes={accountOperationTypes}
@@ -258,7 +221,7 @@ const BalanceHistorySearch = ({
           <div className="flex items-center justify-between mt-10">
             <div>
               <Button
-                onClick={() => handleSearch(true)}
+                onClick={handleSearch}
                 data-testid="apply-filters"
                 disabled={isSearchButtonDisabled}
               >
@@ -271,7 +234,7 @@ const BalanceHistorySearch = ({
               ) : null}
             </div>
             <Button
-              onClick={() => handleFilterClear()}
+              onClick={handleFilterClear}
               data-testid="clear-filters"
             >
               Clear
