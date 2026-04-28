3db2d42c98976e9c4d534e74d1a550973bacfb8c
Dima Rifai <dima.rifai@gmail.com>
2026-04-28 11:11:34 +0300

    Commit the hook

---
+++ hooks/api/accountPage/useAccountBalances.ts
@@ -0,0 +1,27 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+
+import fetchingService from "@/services/FetchingService";
+import Hive from "@/types/Hive";
+
+const useAccountBalances = (
+  accountName: string,
+) => {
+  const {
+    data: accountBalancesData,
+    isLoading: accountBalancesDataLoading,
+    isError: accountBalancesDataError,
+  }: UseQueryResult<Hive.AccountBalancesResponse> = useQuery({
+    queryKey: ["account_balances", accountName],
+    queryFn: () => fetchingService.getAccountBalances(accountName),
+    refetchOnWindowFocus: false,
+    enabled: !!accountName && !!accountName.length,
+  });
+
+  return {
+    accountBalancesData,
+    accountBalancesDataLoading,
+    accountBalancesDataError,
+  };
+};
+
+export default useAccountBalances;
